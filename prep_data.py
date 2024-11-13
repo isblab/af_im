a7c0891be8cce1b1135eb86639d4796e9cbbdcd4
Kenobi98 <majilakartik123@gmail.com>
2024-11-13 12:32:26 +0530

    create and save XLs

--- prep_data.py
+++ prep_data.py
@@ -11,14 +11,17 @@ class DataGathering():
 	def __init__( self ):
 		self.jwalk_exec = "jwalk"
 		self.xl_length = 35
+		self.base_dir = os.path.abspath( "./" )
 
 
 	def forward( self ):
 		for name in ["2ayo"]:
-			self.run_jwalk( f"{name}.pdb" )
-			self.move_to_sys_dir( name )
-			self.parse_jwalk_output( name )
-
+			sys_dir = os.path.abspath( f"./benchmark/{name}/" )
+			# Move to the system dir.
+			os.chdir( sys_dir )
+			self.simulate_xl_data( name )
+			# Back to base.
+			os.chdir( self.base_dir )
 
 
 	def run_jwalk( self, pdb_file: str ):
@@ -41,27 +44,6 @@ class DataGathering():
 		run_subprocess( cmd )
 
 
-	def move_to_sys_dir( self, name ):
-		"""
-		Move Jwalk output to the system dir.
-
-		Input:
-		----------
-		name --> System name.
-
-		Returns:
-		----------
-		None
-		"""
-		jwalk_out = os.path.abspath( "./Jwalk_results" )
-		sys_dir = os.path.abspath( f"./{name}/" )
-		if not os.path.exists( f"{sys_dir}/Jwalk_results/" ):
-			cmd = ["mv", f"{jwalk_out}", f"{sys_dir}"]
-			run_subprocess( cmd )
-		else:
-			print( f"Jwalk_results already present in {name} dir..." )
-
-
 
 	def parse_jwalk_output( self, name: str ):
 		"""
@@ -82,33 +64,63 @@ class DataGathering():
 		----------
 		None
 		"""
-		file_path = glob.glob( f"./benchmark/{name}/Jwalk_results/*.txt" )
+		file_path = glob.glob( f"./Jwalk_results/*.txt" )
 		if len( file_path ) == 0:
-			raise Exception( f"Jwalk_results dir does not exist for {name}..." )
+			raise Exception( f"Jwalk results .txt file does not exist for {name}..." )
 		file_path = file_path[0]
-		df = pd.read_csv( file_path, delim_whitespace = True )
-
-		intraprotein_xls = df[df["Atom1"].str.split( "-" ).str[2] == df["Atom2"].str.split( "-" ).str[2]]
-		interprotein_xls = df[df["Atom1"].str.split( "-" ).str[2] != df["Atom2"].str.split( "-" ).str[2]]
+		df = pd.read_csv( file_path, sep = "\s+" ) # delim_whitespace = True
 
 		# Remove XLs with distances higher than the XL_length.
-		intraprotein_xls = df.loc[df["Euclidean"] <= self.xl_length]
-		interprotein_xls = df.loc[df["Euclidean"] <= self.xl_length]
+		df = df.loc[df["Euclidean"] <= self.xl_length]
+
+		intra = df[df["Atom1"].str.split( "-" ).str[2] == df["Atom2"].str.split( "-" ).str[2]]
+		inter = df[df["Atom1"].str.split( "-" ).str[2] != df["Atom2"].str.split( "-" ).str[2]]
 
 		# Extract chain ID and res no.
+		intraprotein_xls = pd.DataFrame()
+		interprotein_xls = pd.DataFrame()
 		for i in [1, 2]:
-			intraprotein_xls = intraprotein_xls.copy()
-			intraprotein_xls.loc[f"prot{i}"] = intraprotein_xls[f"Atom{i}"].str.split( "-" ).str[2]
-			interprotein_xls = interprotein_xls.copy()
-			interprotein_xls.loc[f"res{i}"] = intraprotein_xls[f"Atom{i}"].str.split( "-" ).str[1]
+			intraprotein_xls[f"prot{i}"] = intra[f"Atom{i}"].str.split( "-" ).str[2]
+			intraprotein_xls[f"res{i}"] = intra[f"Atom{i}"].str.split( "-" ).str[1]
+			interprotein_xls[f"prot{i}"] = inter[f"Atom{i}"].str.split( "-" ).str[2]
+			interprotein_xls[f"res{i}"] = inter[f"Atom{i}"].str.split( "-" ).str[1]
 
-		# Drop the irrelevant columns.
-		drop_cols = ["Index", "Model", "Atom1", "Atom2", "SASD", "Euclidean", "Distance"]
-		intraprotein_xls = intraprotein_xls.drop( drop_cols, axis = 1 )
-		interprotein_xls = interprotein_xls.drop( drop_cols, axis = 1 )
+		intraprotein_xls = intraprotein_xls.reset_index( drop = True )
+		interprotein_xls = interprotein_xls.reset_index( drop = True )
 
+		return intraprotein_xls, interprotein_xls
+
+
+
+	def simulate_xl_data( self, name: str ):
+		"""
+		Simulate XLs for the system uisng Jwalk.
+		Save the intraprotein and interprotein XLs as csv files.
+
+		Input:
+		----------
+		name --> System name.
+
+		Returns:
+		----------
+		None
+		"""
+		# Run Jwalk.
+		if not os.path.exists( f"./Jwalk_results/" ):
+			self.run_jwalk( f"./{name}.pdb" )
+		else:
+			print( f"Jwalk_results already present in {name} dir..." )
+
+		# Obtain intraprotein and interprotein XLs from Jwalk output.
+		intraprotein_xls, interprotein_xls = self.parse_jwalk_output( name )
+		print( f"Intra-XLs = {len( intraprotein_xls )} \t Inter-XLs = {len( interprotein_xls )}" )
+		
+		# Save on disk.
+		intraprotein_xls.to_csv( f"{name}_intraprotein_xls.csv" )
+		interprotein_xls.to_csv( f"{name}_interprotein_xls.csv" )
 
 
 if __name__ == "__main__":
 	DataGathering().forward()
+
 
