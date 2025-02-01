9e6496be51f18589779b020b5b6f829cd4fc301b
Kenobi98 <majilakartik123@gmail.com>
2025-02-01 21:20:35 +0530

    added plot_metrics()

--- create_plots.py
+++ create_plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from typing import Dict
 
+
 def plot_loss( loss_dict: Dict, file_name: str ):
 	"""
 	Plot the per epoch loss.
@@ -31,4 +32,30 @@ def plot_loss( loss_dict: Dict, file_name: str ):
 	plt.close()
 
 
+
+def plot_metrics( metric_dict: Dict, file_name: str ):
+	"""
+	Plot the per epoch loss.
+	"""
+	total = len( metric_dict )
+	col = 2
+	row = math.ceil( total/col )
+
+	fig, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
+
+	c, r = 0, 0
+	for i, k in enumerate( metric_dict.keys() ):
+		loss = metric_dict[k]
+		epochs = np.arange( 0, len( loss ), 1)
+		ax[r].plot( epochs, loss, label = k )
+		ax[r].set_xlabel( "Epochs", fontsize = 16+row*2 )
+		ax[r].set_ylabel( f"{k}", fontsize = 16+row*2 )
+		ax[r].xaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
+		ax[r].yaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
+		
+		r = r+1 if c == 1 else r
+		c = 0 if c == 1 else 1
+
+	plt.savefig( file_name, dpi = 300 )
+	plt.close()
 
