5831b97ee9c9cc5f9884295da475994e58a88753
Kenobi98 <majilakartik123@gmail.com>
2025-02-07 18:11:31 +0530

    common func to create plots from dict

--- create_plots.py
+++ create_plots.py
@@ -7,19 +7,19 @@ import torch
 from typing import Dict
 
 
-def plot_loss( loss_dict: Dict, file_name: str ):
+def create_plot_from_dict( data_dict: Dict, file_name: str ):
 	"""
 	Plot the per epoch loss.
 	"""
-	total = len( loss_dict )
+	total = len( data_dict )
 	col = 2
 	row = math.ceil( total/col )
 
 	fig, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
 
 	c, r = 0, 0
-	for i, k in enumerate( loss_dict.keys() ):
-		loss = loss_dict[k]
+	for i, k in enumerate( data_dict.keys() ):
+		loss = data_dict[k]
 		epochs = np.arange( 0, len( loss ), 1)
 		ax[r, c].plot( epochs, loss, label = k )
 		ax[r, c].set_xlabel( "Epochs", fontsize = 16+row*2 )
@@ -32,6 +32,34 @@ def plot_loss( loss_dict: Dict, file_name: str ):
 
 	plt.savefig( file_name, dpi = 300 )
 	plt.close()
+
+
+
+# def plot_loss( loss_dict: Dict, file_name: str ):
+# 	"""
+# 	Plot the per epoch loss.
+# 	"""
+# 	total = len( loss_dict )
+# 	col = 2
+# 	row = math.ceil( total/col )
+
+# 	fig, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
+
+# 	c, r = 0, 0
+# 	for i, k in enumerate( loss_dict.keys() ):
+# 		loss = loss_dict[k]
+# 		epochs = np.arange( 0, len( loss ), 1)
+# 		ax[r, c].plot( epochs, loss, label = k )
+# 		ax[r, c].set_xlabel( "Epochs", fontsize = 16+row*2 )
+# 		ax[r, c].set_ylabel( f"{k}", fontsize = 16+row*2 )
+# 		ax[r, c].xaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
+# 		ax[r, c].yaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
+		
+# 		r = r+1 if c == 1 else r
+# 		c = 0 if c == 1 else 1
+
+# 	plt.savefig( file_name, dpi = 300 )
+# 	plt.close()
 
 
 
