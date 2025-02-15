6d669d55b6adf9a5afc6dbcf0c037659c4e40ee6
Kenobi98 <majilakartik123@gmail.com>
2025-02-15 14:16:38 +0530

    clean up

--- create_plots.py
+++ create_plots.py
@@ -1,10 +1,12 @@
-import numpy as np
+"""
+This script contains functions to create plots.
+"""
 import math
+from typing import Dict
+import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
-
-from typing import Dict
 
 
 def create_plot_from_dict( data_dict: Dict, file_name: str ):
@@ -15,10 +17,10 @@ def create_plot_from_dict( data_dict: Dict, file_name: str ):
 	col = 2
 	row = math.ceil( total/col )
 
-	fig, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
+	_, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
 
 	c, r = 0, 0
-	for i, k in enumerate( data_dict.keys() ):
+	for k in data_dict:
 		loss = data_dict[k]
 		epochs = np.arange( 0, len( loss ), 1)
 		ax[r, c].plot( epochs, loss, label = k )
@@ -26,40 +28,12 @@ def create_plot_from_dict( data_dict: Dict, file_name: str ):
 		ax[r, c].set_ylabel( f"{k}", fontsize = 16+row*2 )
 		ax[r, c].xaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
 		ax[r, c].yaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
-		
+
 		r = r+1 if c == 1 else r
 		c = 0 if c == 1 else 1
 
 	plt.savefig( file_name, dpi = 300 )
 	plt.close()
-
-
-
-# def plot_loss( loss_dict: Dict, file_name: str ):
-# 	"""
-# 	Plot the per epoch loss.
-# 	"""
-# 	total = len( loss_dict )
-# 	col = 2
-# 	row = math.ceil( total/col )
-
-# 	fig, ax = plt.subplots( row, col, figsize = ( row*8, row*8 ) )
-
-# 	c, r = 0, 0
-# 	for i, k in enumerate( loss_dict.keys() ):
-# 		loss = loss_dict[k]
-# 		epochs = np.arange( 0, len( loss ), 1)
-# 		ax[r, c].plot( epochs, loss, label = k )
-# 		ax[r, c].set_xlabel( "Epochs", fontsize = 16+row*2 )
-# 		ax[r, c].set_ylabel( f"{k}", fontsize = 16+row*2 )
-# 		ax[r, c].xaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
-# 		ax[r, c].yaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
-		
-# 		r = r+1 if c == 1 else r
-# 		c = 0 if c == 1 else 1
-
-# 	plt.savefig( file_name, dpi = 300 )
-# 	plt.close()
 
 
 
@@ -71,10 +45,10 @@ def plot_scalar_metrics( metric_dict: Dict, file_name: str ):
 	col = 2
 	row = math.ceil( total/col )
 
-	fig, ax = plt.subplots( row, 1, figsize = ( row*8, row*8 ) )
+	_, ax = plt.subplots( row, 1, figsize = ( row*8, row*8 ) )
 
 	c, r = 0, 0
-	for i, k in enumerate( metric_dict.keys() ):
+	for k in metric_dict:
 		loss = metric_dict[k]
 		epochs = np.arange( 0, len( loss ), 1)
 		ax.plot( epochs, loss, label = k )
@@ -82,7 +56,7 @@ def plot_scalar_metrics( metric_dict: Dict, file_name: str ):
 		ax.set_ylabel( f"{k}", fontsize = 16+row*2 )
 		ax.xaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
 		ax.yaxis.set_tick_params( labelsize = 14+row, length = 8+row, width = row )
-		
+
 		r = r+1 if c == 1 else r
 		c = 0 if c == 1 else 1
 
@@ -102,8 +76,8 @@ def plot_xl_map( aggregate_map: torch.Tensor, gt_map: torch.Tensor, file_name ) 
 
 	m = aggregate_map != 0
 	print( aggregate_map[m] )
-	
-	fig, ax = plt.subplots( 1, 2, figsize = ( 20, 10 ) )
+
+	_, ax = plt.subplots( 1, 2, figsize = ( 20, 10 ) )
 	ax[0].imshow( aggregate_map )
 	ax[0].set_title( "XLs formed across all epochs" )
 	ax[1].imshow( gt_map )
@@ -111,5 +85,3 @@ def plot_xl_map( aggregate_map: torch.Tensor, gt_map: torch.Tensor, file_name ) 
 
 	plt.savefig( file_name, dpi = 300 )
 	plt.close()
-
-
