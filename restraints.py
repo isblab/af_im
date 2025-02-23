e6619a3783c3f5cd9c28e4e858ad39b12f5e003d
Kenobi98 <majilakartik123@gmail.com>
2025-02-23 12:51:33 +0530

    xl_restraint made modular

--- restraints.py
+++ restraints.py
@@ -24,27 +24,20 @@ class XlRestraint():
 		elif self.config.type == "simple_xlr":
 			return lambda: self.simple_xl_restraint( out, restraint_feature["xl_res_mask"],
 													restraint_feature["xl_max_bound"] )
+
+		# Same as simple_xlr, just modular.
+		elif self.config.type == "mse_xlr":
+			return lambda: self.mse_xl_restraint( out, restraint_feature["xl_res_mask"],
+													restraint_feature["xl_max_bound"] )
 		elif self.config.type == "disto_xlr":
 			return lambda: self.disto_xl_restraint( out, **restraint_feature )
 		else:
 			raise Exception( "At least one of the XL restraint types must be enabled..." )
 
 
-	def fape_xl_restraint( self, out: Dict[str, torch.Tensor],
-							xl_res_mask: torch.tensor,
-							xl_max_bound: float ):
+	def frames_to_dist_map( self, out: Dict[str, torch.Tensor] ):
 		"""
-		Calculate the cross-linking restraint loss as the mean squared deviation for the 
-		predicted Ca distances between the ceoss-linked residues from the max cross-link bound.
-		This is the FAPE implementation for the restraint.
-		This restraint is implemented as a max bound restraint since XLMS 
-			povides a max bound for the distance between XL'd residue pairs.
-
-		XL_restraint = ( D - max_bound )*82 if D > max_bound else 0
-		
-		Note: R - SM recycling dim; B - batch dim (1); N - no. of residues;
-				A - no. of atoms (14 or 37); X - coords dim (3); F - frame dim (4).
-		Taking example of 2ayo; N = 480.
+		Given the predicted rigif frames for the system, compute a distance map.
 		"""
 		# Get the predicted affine matrices.
 		# traj --> [R,N,N,A,F] e.g. For 2ayo, [8,1,480,4,4]
@@ -59,7 +52,7 @@ class XlRestraint():
 			# pred_frames here is an object of class Rigid() which wraps a rotation and translation.
 			pred_aff = Rigid.from_tensor_4x4( traj )
 
-		# This step is not needed as pred_aff is already an object of Rigid().
+		# This step is not needed as pred_aff is already an object of Rigid(). - Kartik -
 		# pred_aff = Rigid(
 		# 	Rotation( 
 		# 		rot_mats = pred_aff.get_rots().get_rot_mats(), 
@@ -81,6 +74,62 @@ class XlRestraint():
 		# 	eps: Krde karam ke dil ye chain paayega
 		pred_dist_map = torch.sqrt( torch.sum( local_pred_pos**2, dim = -1 ) + self.eps )
 
+		return pred_dist_map
+
+
+	def fape_xl_restraint( self, out: Dict[str, torch.Tensor],
+							xl_res_mask: torch.tensor,
+							xl_max_bound: float ):
+		"""
+		Calculate the cross-linking restraint loss as the mean squared deviation for the 
+		predicted Ca distances between the ceoss-linked residues from the max cross-link bound.
+		This is the FAPE implementation for the restraint.
+		This restraint is implemented as a max bound restraint since XLMS 
+			povides a max bound for the distance between XL'd residue pairs.
+
+		XL_restraint = ( D - max_bound )*82 if D > max_bound else 0
+		
+		Note: R - SM recycling dim; B - batch dim (1); N - no. of residues;
+				A - no. of atoms (14 or 37); X - coords dim (3); F - frame dim (4).
+		Taking example of 2ayo; N = 480.
+		"""
+		# # Get the predicted affine matrices.
+		# # traj --> [R,N,N,A,F] e.g. For 2ayo, [8,1,480,4,4]
+		# traj = out["sm"]["frames"]
+
+		# # Legacy: not required but keeping OpenFold implementation.
+		# # Need to check if the traj belongs to 4*4 matrix or a tensor_7
+		# if traj.shape[-1] == 7:
+		# 	pred_frames = Rigid.from_tensor_7( traj )
+		# elif traj.shape[-1] == 4:
+		# 	# OpenFold uses a rotation matrix.
+		# 	# pred_frames here is an object of class Rigid() which wraps a rotation and translation.
+		# 	pred_aff = Rigid.from_tensor_4x4( traj )
+
+		# # This step is not needed as pred_aff is already an object of Rigid(). - Kartik -
+		# # pred_aff = Rigid(
+		# # 	Rotation( 
+		# # 		rot_mats = pred_aff.get_rots().get_rot_mats(), 
+		# # 		quats = None ),
+		# # 		pred_aff.get_trans(),
+		# # 	)
+
+		# # The translation vector of the affine matrix is used as positions.
+		# pred_positions = pred_aff.get_trans()
+
+		# # Apply the affine transformation to obtain the predicted positions in local frame.
+	    # # This directly gives us the pairwise distances for each residue along xyz.
+	    # # local_pred_pos --> [R,B,N,N,X] or [8,1,480,480,3]
+		# local_pred_pos = pred_aff.invert()[..., None].apply(
+		# 	pred_positions[..., None, :, :],
+		# )
+		
+		# # Calculate the pairwise Euclidean distance matrix.
+		# # 	eps: Krde karam ke dil ye chain paayega
+		# pred_dist_map = torch.sqrt( torch.sum( local_pred_pos**2, dim = -1 ) + self.eps )
+
+		pred_dist_map = self.frames_to_dist_map( out )
+
 		# Adjust the length scales.
 		pred_dist_map = pred_dist_map / self.length_scale
 		xl_max_bound = xl_max_bound / self.length_scale
@@ -101,13 +150,114 @@ class XlRestraint():
 		return loss
 
 
-	def simple_xl_restraint( self, out: Dict[str, torch.Tensor],
-							xl_res_mask: torch.tensor,
-							xl_max_bound: float ):
+	def final_pred_to_dist_map( self, final_atom_pos: torch.Tensor
+								) -> torch.Tensor:
 		"""
-		Calculate the cross-linking restraint loss as the mean squared deviation for the 
+		Compute a distance map from the final_atom_positions.
+		Using only Ca-coordinates for distance map calculation.
+
+		Input:
+		----------
+		final_atom_pos --> coordinates in atom37 representtaion.
+						[B,N,37,3] --> For 2ayo: [1,480,37,3]
+
+		Returns:
+		----------
+		D --> Ca-distance map [B,N,N].
+		"""
+		# Extracting Ca-coordinates - index 1.
+		# [B,N,3] --> For 2ayo: [1,480,3]
+		ca_pos = final_atom_pos[..., 1, :]
+		diff = ca_pos.unsqueeze( 2 ) - ca_pos.unsqueeze( 1 )
+		D = torch.sqrt( 
+						torch.sum( ( diff )**2, dim = -1 ) + self.eps
+						)
+
+		# Adjust the length scales.
+		scaled_D = D / self.length_scale
+		return scaled_D
+
+
+	def get_violations_mask( self, D: Dict[str, torch.Tensor],
+							xl_res_mask: torch.tensor,
+							scaled_xl_max_bound: float ) -> torch.Tensor:
+		"""
+		Adjust the length sacales for the distance map and xl_max_bound.
+		Identify XL violations.
+
+		Input:
+		----------
+		D --> Ca-distance map [B,N,N].
+		xl_res_mask --> binary mask indicating cross-linked residue pairs.
+		xl_max_bound --> max distance between the cross-licked residues 
+						scaled by self.length_scale.
+
+		Returns:
+		----------
+		viols_mask --> bool mask indicating violated XLs.
+		"""
+		# Consider only the cross-linked residues.
+		D = D*xl_res_mask
+
+		# Identify Xl violations.
+		viols_mask = D > xl_max_bound
+		return viols_mask
+
+
+	def mse_xl_restraint( self, out: Dict[str, torch.Tensor],
+							xl_res_mask: torch.tensor,
+							xl_max_bound: float ) -> torch.Tensor:
+		"""
+		Calculate the cross-linking restraint loss as the mean squared error (MSE) for the 
 		predicted Ca distances between the ceoss-linked residues from the max cross-link bound.
 		Here, I am using the "final_atom_positions" for the restraint.
+
+		Input:
+		----------
+		out --> output dict from the model.
+		xl_res_mask --> binary mask indicating cross-linked residue pairs.
+		xl_max_bound --> max distance between the cross-licked residues.
+
+		Returns:
+		----------
+		loss --> mean squared loss.
+		"""
+		D = self.final_pred_to_dist_map( out["final_atom_positions"] )
+
+		# Adjust the length scales.
+		scaled_xl_max_bound = xl_max_bound / self.length_scale
+		
+		viols_mask = self.get_violations_mask( D, xl_res_mask, scaled_xl_max_bound )
+
+		# Loss is computed only for the violated XLs.
+		if viols_mask.any():
+			squared_diff = ( D[viols_mask] - scaled_xl_max_bound )**2
+		else:
+			squared_diff = D*0 
+		
+		# Normalizing by the total no. of cross-linked residue pairs.
+		denom = self.eps + torch.sum( xl_res_mask )
+		mse = torch.sum( squared_diff )/ denom
+		return mse
+
+
+	def simple_xl_restraint( self, out: Dict[str, torch.Tensor],
+							xl_res_mask: torch.tensor,
+							xl_max_bound: float ) -> torch.Tensor:
+		"""
+		Calculate the cross-linking restraint loss as the mean squared error (MSE) for the 
+		predicted Ca distances between the ceoss-linked residues from the max cross-link bound.
+		Here, I am using the "final_atom_positions" for the restraint.
+
+		Input:
+		----------
+		out --> output dict from the model.
+		xl_res_mask --> binary mask indicating cross-linked residue pairs.
+		xl_max_bound --> max distance between the cross-licked residues.
+
+		Returns:
+		----------
+		loss --> mean squared loss.
 		"""
 		# [B,N,37,3] --> For 2ayo: [1,480,37,3]
 		pred_positions = out["final_atom_positions"]
@@ -119,6 +269,7 @@ class XlRestraint():
 		D = torch.sqrt( 
 						torch.sum( ( diff )**2, dim = -1 ) + self.eps
 						)
+		
 		# Adjust the length scales.
 		D = D / self.length_scale
 		xl_max_bound = xl_max_bound / self.length_scale
@@ -129,15 +280,16 @@ class XlRestraint():
 		# Identify Xl violations.
 		viols_mask = D > xl_max_bound
 		if viols_mask.any():
-			loss = ( D[viols_mask] - xl_max_bound )**2
+			squared_diff = ( D[viols_mask] - xl_max_bound )**2
 		else:
 			# loss = torch.tensor( [0.0], device = D.device, requires_grad = True )
-			loss = D*0 
+			squared_diff = D*0 
 		
 		# Normalizing by the total no. of cross-linked residue pairs.
 		denom = self.eps + torch.sum( xl_res_mask )
-		loss = torch.sum( loss )/ denom
-		return loss
+		mse = torch.sum( squared_diff )/ denom
+		return mse
+
 
 
 	def disto_xl_restraint( self, out: Dict[str, torch.Tensor],
