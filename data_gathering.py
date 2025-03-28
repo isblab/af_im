d3336f7ef1924f6755598e2c866b86df288ba932
Kenobi98 <majilakartik123@gmail.com>
2025-03-28 20:02:16 +0530

    working to account for ambiguity

--- data_gathering.py
+++ data_gathering.py
@@ -42,15 +42,20 @@ class DataGathering():
 		Given the system specific configs,
 			1. Create the full system containing all chains to be modeled.
 			2. Create a directory containing FASTA file for input to OpenFold.
-			3. Create restraint features dict.
+			3. Create a mapping for residue positions to system indices.
+			4. Create a mapping for entity to chains.
+			5. Create restraint features dict.
 		"""
 		system_dict = self.create_full_system()
 		self.create_fasta_input( system_dict )
+		self.res_idx_map = self.create_residue_index_mapping( system_dict )
+		self.entity_chain_map = self.create_entity_chain_mapping( system_dict )
 		self.create_xl_restraint_features( system_dict )
 
 
-
-	def get_chain_id( self, idx: str ):
+	#--------------------------------------------------------------------#
+	#--------------------------------------------------------------------#
+	def get_chain_id( self, idx: str ) -> str:
 		"""
 		Get a chain ID based on an index.
 		"""
@@ -64,14 +69,17 @@ class DataGathering():
 			raise Exception( "Too many chains." )
 
 
-	def create_full_system( self ):
+	def create_full_system( self ) -> Dict[str, Dict]:
 		"""
 		Given the entities in the system configs and the copy no.,
 			create all chains in the system (dict) to be modeled.
+		Each chain in the system is identified by a sys_chain_id:
+			"{sys_name}_{entity_id}_{chain_id}"
 		"""
 		system_dict = {}
 		idx = 0
 		for entity in self.sys_config.entity:
+			entity_id = entity["entity_id"]
 			copy_num = entity["copy_num"]
 			start = entity["start"]
 			end = entity["end"]
@@ -80,7 +88,7 @@ class DataGathering():
 			for i in range( copy_num ):
 				chain_id = self.get_chain_id( idx )
 
-				system_dict[f"{self.sys_name}_{entity}_{chain_id}"] = {
+				system_dict[f"{self.sys_name}_{entity_id}_{chain_id}"] = {
 													"seq": seq,
 													"positions": [start, end]
 				}
@@ -89,6 +97,8 @@ class DataGathering():
 		return system_dict
 
 
+	#--------------------------------------------------------------------#
+	#--------------------------------------------------------------------#
 	def create_fasta_input( self, system_dict: Dict ):
 		"""
 		Given the full system to be modeled, create a 
@@ -102,17 +112,10 @@ class DataGathering():
 				w.writelines( f">{chain}\n{system_dict[chain]['seq']}\n" )
 
 
-	def parse_xl_data( self ):
-		"""
-		Parse the .csv file containing the XL data.
-		"""
-		# xl_df = pd.read_csv( os.path.abspath( "2ayo_interprotein_xls.csv" ) )
-		xl_df = pd.read_csv( os.path.abspath( self.sys_config["data_gathering"]["xl_restraint"]["file_name"] ) )
-
-		return xl_df
-
-
-	def get_residue_index_map( self, system_dict: Dict ):
+	#--------------------------------------------------------------------#
+	#--------------------------------------------------------------------#
+	def create_residue_index_mapping( self, system_dict: Dict
+										) -> Dict[str, Dict[int, int]]:
 		"""
 		Create a mapping between the residue position to system indices and its inverse.
 		Also map the residue positions to the corresponding chains.
@@ -145,26 +148,35 @@ class DataGathering():
 		return res_idx_map
 
 
-	def map_residue_to_index( self, xl_df: pd.DataFrame, res_idx_map: Dict ):
+	def create_entity_chain_mapping( self, system_dict: Dict
+									) -> Dict[int, List]:
 		"""
-		Given a DataFrame containing positions for cross-linked residues,
-			map all residue positions to system indices from 0 to N,
-			where N is the total no. of residues in the system.
+		Given the system_dict, map all chains to the corresponding entities.
 		"""
-		for i in range( len( xl_df ) ):
-			chain1 = xl_df.loc[ i, "prot1" ]
-			res1 = xl_df.loc[ i, "res1" ]
-			index1 = res_idx_map[chain1]["res_to_ind"][res1]
-			xl_df.loc[ i, "res1" ] = index1
-			
-			chain2 = xl_df.loc[ i, "prot2" ]
-			res2 = xl_df.loc[ i, "res2" ]
-			index2 = res_idx_map[chain2]["res_to_ind"][res2]
-			xl_df.loc[ i, "res2" ] = index2
+		entity_chain_map = {}
+		for sys_chain_id in system_dict:
+			sys_name, entity_id, chain_id = sys_chain_id.split( "_" )
+			entity_id = int( entity_id )
+			if entity_id in entity_chain_map:
+				entity_chain_map[entity_id].append( chain_id )
+			else:
+				entity_chain_map[entity_id] = [chain_id]
+
+		return entity_chain_map
+
+
+	#--------------------------------------------------------------------#
+	#--------------------------------------------------------------------#
+	def parse_xl_data( self ) -> pd.DataFrame:
+		"""
+		Parse the .csv file containing the XL data.
+		"""
+		# xl_df = pd.read_csv( os.path.abspath( "2ayo_interprotein_xls.csv" ) )
+		xl_csv_file = self.sys_config["data_gathering"]["xl_restraint"]["file_name"]
+		xl_df = pd.read_csv( os.path.abspath( xl_csv_file ) )
 
 		return xl_df
 
-;
 
 	def get_sys_len( self, system_dict: Dict ):
 		"""
@@ -175,6 +187,49 @@ class DataGathering():
 			sys_len += len( system_dict[chain]['seq'] )
 
 		return sys_len
+
+
+	def map_residue_to_index( self, xl_df: pd.DataFrame ) -> pd.DataFrame:
+		"""
+		Given a DataFrame containing positions for cross-linked residues,
+			map all residue positions to system indices from 0 to N,
+			where N is the total no. of residues in the system.
+		"""
+		for i in range( len( xl_df ) ):
+			chain1 = xl_df.loc[ i, "prot1" ]
+			res1 = xl_df.loc[ i, "res1" ]
+			index1 = self.res_idx_map[chain1]["res_to_ind"][res1]
+			xl_df.loc[ i, "res1" ] = index1
+			
+			chain2 = xl_df.loc[ i, "prot2" ]
+			res2 = xl_df.loc[ i, "res2" ]
+			index2 = self.res_idx_map[chain2]["res_to_ind"][res2]
+			xl_df.loc[ i, "res2" ] = index2
+
+		return xl_df
+
+
+	def get_chains_for_entity( self, prot: str ) -> List:
+		"""
+		Given a protein name ("prot_{entity_id}"), return all chains for the entity.
+		"""
+		_, entity_id = prot.split( "_" )
+		entity_id = int( entity_id )
+		chains = self.entity_chain_map[entity_id]
+		return chains
+
+
+	def account_ambiguity( self, xl_df: pd.DataFrame ):
+		"""
+		To account for ambiguity we consider all inter-chain homomeric interactions.
+		"""
+		for i in range( xl_df.shape[0] ):
+			prot1 = xl_df.iloc[i,0]
+			chains1 = self.get_chains_for_entity( prot1 )
+			prot2 = xl_df.iloc[i,2]
+			chains2 = self.get_chains_for_entity( prot2 )
+
+
 
 
 	def create_xl_restraint_features( self, system_dict ):
@@ -194,9 +249,9 @@ class DataGathering():
 		# xl_df = self.add_offsets( offset_dict, xl_df )
 
 		sys_len = self.get_sys_len( system_dict )
-		res_idx_map = self.get_residue_index_map( system_dict )
+		# res_idx_map = self.create_residue_index_mapping( system_dict )
 
-		xl_df = self.map_residue_to_index( xl_df, res_idx_map )
+		xl_df = self.map_residue_to_index( xl_df )
 		print( "Total XL pairs = ", len( xl_df ) )
 
 		# Create a 0-matrix for the XL-residue mask [r,r].
