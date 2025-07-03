e974b112b1d418c97f989139056157ee6cc90c0e
Kenobi98 <majilakartik123@gmail.com>
2025-07-03 17:47:17 +0530

    minor modifications

--- system_representation.py
+++ system_representation.py
@@ -56,12 +56,12 @@ from utils.utils import run_subprocess
 
 
 class SystemRepresentation():
-	def __init__( self, sys_name: str, ofold_dir: str, ofold_script: str, 
+	def __init__( self, sys_name: str,
+					ofold_dir: str, ofold_script: str, 
 					fasta_dir: str, alignment_dir: str, 
 					ofold_output_dir: str, config_preset: str, 
 					ckpt_path: Optional[str], mode: str,
 					init_model_prefix: str,
-					# init_struct_pdb: str, init_struct_cif: str,
 					cpu_cores: int, seed_worker, device: str = "cpu" ):
 		self.sys_name = sys_name
 		self.openfold_dir = ofold_dir
@@ -80,18 +80,22 @@ class SystemRepresentation():
 
 
 	def forward( self ):
-		if len( glob.glob( f"{self.ofold_output_dir}/predictions/*{self.init_model_prefix}.cif" ) ) != 0:
+
+		init_model_path = glob.glob(
+				f"{self.ofold_output_dir}/predictions/*{self.init_model_prefix}.cif"
+				)
+		if len( init_model_path ) != 0:
 			print( "\nInitial structure for the system exists..." )
 			print( f"Using {self.init_model_prefix} model as inital model..." )
-			self.init_struct_cif = glob.glob( f"{self.ofold_output_dir}/predictions/*{self.init_model_prefix}.cif" )[0]
+			self.init_struct_cif = init_model_path[0]
 		
 		else:
 			print( "\nPredicting the initial structure for the system..." )
-			if not os.path.exists( os.path.abspath( f"{self.ofold_output_dir}/predictions/*{self.init_model_prefix}.cif" ) ):
-				self.get_initial_structure()
+			if not os.path.exists( os.path.abspath( init_model_path ) ):
+				self.predict_initial_structure()
 			# OpenFold predicted initial structure in CIF format.
 			print( f"Using {self.init_model_prefix} model as inital model..." )
-			self.init_struct_cif = glob.glob( f"{self.ofold_output_dir}/predictions/*{self.init_model_prefix}.cif" )[0]
+			self.init_struct_cif = init_model_path[0]
 
 		print( "\nCreating ground truth features from the initial structure..." )
 		data = self.get_feature_from_init_struct()
@@ -102,7 +106,7 @@ class SystemRepresentation():
 		return data
 
 
-	def get_initial_structure( self ):
+	def predict_initial_structure( self ):
 		"""
 		Run monomer/multimer prediction based on mode.
 		openfold/script_utils.run_model has been modified ot save the 
@@ -139,79 +143,6 @@ class SystemRepresentation():
 		print( f"Running {mode} prediction..." )
 		command = obj.get()
 		run_subprocess( command )
-		# if self.mode == 'mono':
-		# 	self.run_monomer_prediction()
-		# elif self.mode == 'multi':
-		# 	self.run_multimer_prediction()
-
-
-	def run_monomer_prediction( self ):
-		"""
-		Use OpenFold for monomer structure prediction.
-			Use precomputed alignments if available.
-
-		Input:
-		----------
-		training --> (bool) specifies if OpenFold is to be used for training/prediction.
-
-		Returns:
-		----------
-		None
-		"""
-
-		# if training:
-		# 	script = "unfrozen_pretrained.py"
-		# else:
-		# 	script = "run_pretrained_openfold.py"
-
-		# Move to OpenFold dir.
-		os.chdir( self.openfold_dir )
-		
-		if os.path.exists( self.alignment_dir ):
-			print( "using precomputed alignments for monomer prediction..." )
-			command = monomer_precomp_aln_cmd( self.script, self.fasta_dir, 
-											self.alignment_dir, self.ofold_output_dir,
-											self.config_preset, self.ckpt_path,
-											self.cpu_cores, self.device )
-
-		else:
-			command = monomer_cmd( self.script, self.fasta_dir, self.ofold_output_dir,
-									self.config_preset,  self.ckpt_path,
-									self.cpu_cores, self.device )
-
-		print( "Running monomer prediction..." )
-		run_subprocess( command )
-
-
-
-	def run_multimer_prediction( self ):
-		"""
-		Use OpenFold for multimer structure prediction.
-			Use precomputed alignments if available.
-
-		Input:
-		----------
-		training --> (bool) specifies if OpenFold is to be used for training/prediction.
-
-		Returns:
-		----------
-		None
-		"""
-		# Move to OpenFold dir.
-		os.chdir( self.openfold_dir )
-
-		if os.path.exists( self.alignment_dir ):
-			print( "using precomputed alignments for multimer prediction..." )
-			command = multimer_precomp_aln_cmd( self.script, self.fasta_dir, 
-												self.alignment_dir, self.ofold_output_dir,
-												self.config_preset, self.cpu_cores, self.device )
-
-		else:
-			command = multimer_cmd( self.script, self.fasta_dir, self.ofold_output_dir,
-									self.config_preset, self.cpu_cores, self.device )
-
-		print( "Running multimer prediction..." )
-		run_subprocess( command )
 
 
 	def get_feature_from_init_struct( self ):
@@ -221,7 +152,7 @@ class SystemRepresentation():
 		    mmcif_string = f.read()
 
 		mmcif_object = parse( 
-		    file_id = "2ayo", mmcif_string = mmcif_string
+		    file_id = self.sys_name, mmcif_string = mmcif_string
 		).mmcif_object
 
 		print( os.path.exists( self.init_struct_cif ) )
@@ -229,7 +160,7 @@ class SystemRepresentation():
 		np_example = process_mmcif( mmcif_object )
 
 		print( "Load config file..." )
-		config = model_config( "model_1_multimer_v3" )
+		config = model_config( self.config_preset )
 
 		# Obtain the ground truth features.
 		data = np_example_to_features(
