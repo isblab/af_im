07f01763f7c7b53b2e4e02ab5613fcb78cb62fda
Kenobi98 <majilakartik123@gmail.com>
2025-07-15 16:44:16 +0530

    logging time and memory

--- eye_drop.py
+++ eye_drop.py
@@ -5,66 +5,90 @@ Filters out complexes that:
 	Have >cutoff data satisfaction.
 """
 from typing import Dict
-import os, glob, time, subprocess
+import os, glob, time, subprocess, time
+import ml_collections as mlc
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
-from scipy.spatial import distance_matrix
 from openfold_wrapper import IntegrativeLearning
 
 from topology import topology_dict
-from utils.utils import ( open_file_handler, run_subprocess )
+from utils.utils import ( open_file_handler,
+						read_json,
+						write_json,
+						run_subprocess )
 
 
 class InitPrediction():
 	def __init__( self ):
-		self.benchmark_name = "afu"
+		self.benchmark_name = "afu"  # afu/ sabdab
 		# Define the modeling objective.
-		self.modeling_objective = "Obtaining initial prediction."
+		self.modeling_objective = f"({self.benchmark_name}) Obtaining initial prediction."
 		self.modeling_version = 0
 
-		self.base_dir = os.path.join( os.path.abspath( "./benchmark/" ) )
-		self.meta_dir = os.path.join( self.base_dir,
-											f"{self.benchmark_name}_metadata" )
-		# Name for the dir to store modeling output for all systems.
-		self.modeling_dir_name = f"{self.benchmark_name}_modeling"
+		self.logs = {}
 
-		self.benchmark = pd.read_csv( os.path.join( self.meta_dir,
-							f"{self.benchmark_name}_benchmark.csv" )
-							)
-
-		# self.xl_benchmark = ["8gtj", '8i2f', "7qot", "8dwl","7xad",
-		# 					"8phv", "8cxj", "8g9p", "8pfc", "8gt0",
-		# 					"8gtk", "8bzr", "8h8a", "7wr6", "7ymf",
-		# 					"7xvk", "8b3s", "7xvo", "8odr", "8gxe",
-		# 					"8t1c", "8wtd"]
-		self.num_systems = self.benchmark.shape[0]
-
-		self.selected_benchmark_file = os.path.join(
-										self.meta_dir,
-										f"selected_{self.benchmark_name}_benchmark.csv"
-										)
 
 
 	def forward( self ):
 		"""
 		"""
-		# self.create_dirs()
+		self.create_required_paths()
+		self.initialize_logs_dict()
+		self.load_benchamrk()
 		self.run_modeling_for_benchmark()
 
 		self.filter_benchmark()
 
+		write_json( self.logs, self.logs_file )
 
-	# def create_dirs( self ):
-	# 	"""
-	# 	Craete the required directories.
-	# 	"""
-	# 	os.makedirs( self.modeling_dir, exist_ok = True )
+
+	## ------------------------------------------------------ ##
+	## ------------------------------------------------------ ##
+	def initialize_logs_dict( self ):
+		"""
+		Log the following info:
+			Time taken by each system.
+			Memory consumed by each system.
+		"""
+		if os.path.exists( self.logs_file ):
+			self.logs = read_json( self.logs_file )
+		else:
+			self.logs = {k:{} for k in ["time", "memory"]}
+
+
+	def create_required_paths( self ):
+		"""
+		Create all the required paths.
+		"""
+		self.base_dir = os.path.join( os.path.abspath( "./benchmark/" ) )
+		self.meta_dir = os.path.join( self.base_dir,
+							f"{self.benchmark_name}_metadata" )
+		# Name for the dir to store modeling output for all systems.
+		self.modeling_dir_name = f"{self.benchmark_name}_modeling"
+
+		self.benchmark_file = os.path.join( self.meta_dir,
+						f"{self.benchmark_name}_benchmark.csv" )
+
+		self.selected_benchmark_file = os.path.join( self.meta_dir,
+						f"selected_{self.benchmark_name}_benchmark.csv" )
+		self.logs_file = os.path.join( self.meta_dir,
+						f"Logs_init_model_{self.benchmark_name}.json" )
+
+
+	## ------------------------------------------------------ ##
+	## ------------------------------------------------------ ##
+	def load_benchamrk( self ):
+		"""
+		Load the benchmak .csv file.
+		Note the total no. of systems to be modeled.
+		"""
+		self.benchmark = pd.read_csv( self.benchmark_file )
+		self.num_systems = self.benchmark.shape[0]
 
 
 	def get_sys_path( self, sys_name: str ):
-		# sys_path = os.path.abspath( f"./benchmark/modeling/{sys_name}/" )
 		sys_path = os.path.join( 
 					os.path.abspath( f"{self.base_dir}/{self.modeling_dir_name}/{sys_name}" )
 			)
@@ -88,6 +112,42 @@ class InitPrediction():
 		return stats_dict
 
 
+	## ------------------------------------------------------ ##
+	## ------------------------------------------------------ ##
+	def log_memory_usage( self, sys_name: str, device: str ):
+		"""
+		Log the device memory used during modeling.
+		device must be in the following formar: cuda[0]
+		Reset the CUDA memory stats after logging.
+		"""
+		if device == "cpu":
+			raise valueError( "Pytorch does not provide " +
+					"built-in functions to check CPU memory stats. " +
+					"Change device to cuda[0/1]..." )
+		else:
+			device_num = int( device[-1] )
+		# get peak memory since last reset.
+		max_allocated = torch.cuda.max_memory_reserved( device_num )
+		max_reserved = torch.cuda.max_memory_reserved( device_num )
+		self.logs["memory_allocated"][sys_name] = max_allocated
+		self.logs["memory_reserved"][sys_name] = max_reserved
+
+		torch.cuda.reset_peak_memory_stats( device_num )
+
+
+	def modify_topology( self, sys_name: str ):
+		"""
+		Add modeling objective and version to topology for each system.
+		"""
+		topo_dict = topology_dict()
+		topo_dict.objective = f"{sys_name} {self.modeling_objective}"
+		topo_dict.train.version = self.modeling_version
+
+		return topo_dict
+
+
+	## ------------------------------------------------------ ##
+	## ------------------------------------------------------ ##
 	def run_modeling_for_benchmark( self ):
 		"""
 		Run the Integrative modeling pipeline for the entire benchmark.
@@ -95,31 +155,39 @@ class InitPrediction():
 		print( "\033[1mRunning modeling for the benchmark...\033[0m" )
 		curr_dir = os.getcwd()
 		for idx, sys_name in enumerate( self.benchmark["PDB ID"] ):
-			print( "\n------------------------------------------------------------" )
-			print( "------------------------------------------------------------" )
-			print( f"{idx}/{self.num_systems} --> {sys_name}" )
-			print( "------------------------------------------------------------" )
-			print( "------------------------------------------------------------\n" )
+			print( "\n" + "-"*70 + "\n" + "-"*70 )
+			print( "-"*25 + f" {idx}/{self.num_systems} --> {sys_name} " + "-"*25 )
+			print( "\n" + "-"*70 + "\n" + "-"*70 )
 
 			# 7xpc -> Error in res_idx_map[chain1]
-			if sys_name in ["7ui8", "7qot"]:  # "7xpc"
-				continue
+			# if sys_name in ["7ui8", "7qot"]:  # "7xpc"
+				# continue
 			sys_path = self.get_sys_path( sys_name )
 			stat_file_path = self.get_stat_file_path( sys_path )
 
 			if not os.path.exists( stat_file_path ):
-				self.run_modeling_for_system( sys_name )
+				tic = time.time()
+				topo_dict = self.modify_topology( sys_name = sys_name )
+				device = topo_dict.train.device
+				self.run_modeling_for_system( sys_name = sys_name,
+												topo_dict = topo_dict )
+				toc = time.time()
+				self.logs["time"][sys_name] = toc-tic
+				self.log_memory_usage()
+
 			else:
 				print( f"Summary file already present for {sys_name}..." )
 
+			write_json( self.logs, self.logs_file )
+
 			# Return to base_dir.
 			os.chdir( curr_dir )
-			fasta_path = f"./benchmark/fasta_dir/{sys_name}.fasta"
+			fasta_path = f"./benchmark/{sys_name}_fasta/"
 			if os.path.exists( fasta_path ):
 				run_subprocess( ["rm", f"{fasta_path}"] )
 
 
-	def run_modeling_for_system( self, sys_name: str ):
+	def run_modeling_for_system( self, sys_name: str, topo_dict: mlc.ConfigDict ):
 		"""
 		Run the Integrative modeling pipeline for a give system.
 		If summary file exists fo a run, do not run again.
@@ -131,9 +199,7 @@ class InitPrediction():
 								f"{self.benchmark_name}_benchmark/{sys_name}/" )
 		# fasta_path = "./benchmark/"
 
-		topo_dict = topology_dict()
-		topo_dict.objective = f"{sys_name} {self.modeling_objective}"
-		topo_dict.train.version = self.modeling_version
+
 
 		il_obj = IntegrativeLearning( 
 				sys_name = sys_name,
@@ -149,6 +215,8 @@ class InitPrediction():
 		torch.cuda.empty_cache()
 
 
+	## ------------------------------------------------------ ##
+	## ------------------------------------------------------ ##
 	def get_summary_dict( self ):
 		"""
 		Return an empty summary dict containing all required keys.
@@ -182,7 +250,7 @@ class InitPrediction():
 
 
 
-	def get_system_data( self, sys_name: str ):
+	def get_system_data( self, sys_name: str ) -> Dict:
 		"""
 		Get the following info for all complexes:
 			PDB ID (sys_name)
