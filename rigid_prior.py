fe76eb3ec371d0fa09d58479791a68090b24611b
Kenobi98 <majilakartik123@gmail.com>
2025-08-14 10:07:59 +0530

    improved versioning of modeling runs

--- rigid_prior.py
+++ rigid_prior.py
@@ -1,13 +1,15 @@
 """
 Test whether AF2 learns a rigid prior or not.
 Wrapper over the BenchmarkModeling module.
+	Here, I do not run the analyis module and also
+		switch off creating plots in BenchmarkModeling module.
 """
 from typing import List, Tuple, Dict
 import os, glob, pickle as pkl
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 
 from benchmark_runs import BenchmarkModeling
 from utils.paths import (
@@ -22,6 +24,11 @@ from utils.tools import (
 	compute_rmsf_wrt_first_model,
 	compute_rmsd_post_align )
 
+"""
+Using a fixed set of xl weights. Each corresponds to a modeling version.
+For subsequent experiments with loss and XL type, using sub_version
+	to distinguish the runs and keep my sanity intact.
+"""
 
 class RigigPrior():
 	"""
@@ -30,13 +37,14 @@ class RigigPrior():
 	"""
 	def __init__( self ):
 		self.benchmark_name = "rigid"
-		self.xl_restraint_weights = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
+		self.xl_restraint_weights = [1e-8, 1e-6, 1e-4, 1e-3, 1e-2]
+		self.sub_version = 0.1
+		# Provide shorthand for the loss to be included.
+		self.include_loss = ""
+		# File format to save the structure.
 		self.struct_format = "pdb"
-		self.sys_conf_suff = "_1tp"
-		if self.sys_conf_suff == "_1fp":
-			self.modeling_version_range = {"no_viol": [1, 7], "viol": [8, 14]}
-		elif self.sys_conf_suff == "_1tp":
-			self.modeling_version_range = {"no_viol": [15, 21], "viol": [22, 28]}
+		# Suffix for the sys_config and xls file.
+		self.sys_conf_suff = "_1fp"
 		self.remove_sys_modeling_dir = False
 
 
@@ -45,20 +53,14 @@ class RigigPrior():
 		"""
 		self.create_required_paths()
 		self.load_benchmark()
+		# self.get_modeling_versions()
 
-		num_xl_weights = len( self.xl_restraint_weights )
-		start1, end1 = self.modeling_version_range["no_viol"]
-		start2, end2 = self.modeling_version_range["viol"]
-		modeling_versions = {
-			"no_viol": list( np.arange( start1, end1 + 1, 1 ) ),
-			"viol": list( np.arange( start2, end2 + 1, 1 ) )
-		}
-
-		self.run_modeling( modeling_versions = modeling_versions )
-		self.plot_modeling_results( modeling_versions = modeling_versions )
+		for sv, loss in zip( [0, 0.1, 0.2, 0.3, 0.4, 0.5], ["", "v", "i", "s", "vi", "vs"] ):
+			self.sub_version = sv
+			self.include_loss = loss
+			self.run_modeling()
 
 		# self.convert_ensemble_to_gif()
-
 
 	################################################################################
 	################################################################################
@@ -77,10 +79,22 @@ class RigigPrior():
 			self.meta_dir,
 			f"selected_{self.benchmark_name}_benchmark.csv" )
 
-		# Path to the VMD script.
-		self.vmd_script = os.path.join(
-			os.path.abspath( "./utils/vmd_visualize_traj.tcl" )
-			)
+		# this dir stores all the plots created by this script.
+		# 	Separates from the benchmark_results directory.
+		self.rigid_prior_results_dir = os.path.join(
+			self.base_dir,
+			"rigid_prior_plots" )
+		os.makedirs( self.rigid_prior_results_dir, exist_ok = True )
+
+
+	def create_plots_dir( self ):
+		"""
+		Create the required directories.
+		"""
+		self.plots_dir = os.path.join(
+			self.rigid_prior_results_dir,
+			f"plots{self.sys_conf_suff}" )
+		os.makedirs( self.plots_dir, exist_ok = True )
 
 
 	def load_benchmark( self ):
@@ -89,57 +103,114 @@ class RigigPrior():
 		"""
 		self.benchmark = pd.read_csv( self.benchmark_csv_file )
 
+
+	def get_modeling_versions( self ):
+		"""
+		Create a dict containing the version for the modeling run.
+		"""
+		if self.sys_conf_suff == "_1fp":
+			start, end = 1, 5
+		elif self.sys_conf_suff == "_1tp":
+			start, end = 6, 10
+
+		self.modeling_versions = np.arange(
+			start+self.sub_version, end+self.sub_version + 1, 1
+			)
+
 	################################################################################
 	################################################################################
-	def run_modeling( self, modeling_versions: Dict[str, List] ):
+	def run_modeling( self ):
 		"""
 		Run modeling for all required parameters.
 		"""
-		modeling_version = 1
-		for viol_label in modeling_versions:
-			if viol_label == "no_viol":
-				enable_violation_loss = False
-			elif viol_label == "viol":
-				enable_violation_loss = True
-
+		for suff in ["_1fp", "_1tp"]:
+			self.sys_conf_suff = suff
+			print( f"Config file suffix: {self.sys_conf_suff}..." )
+			self.create_plots_dir()
+			self.get_modeling_versions()
+			print( self.modeling_versions )
 			for i, xl_weight in enumerate( self.xl_restraint_weights ):
-				modeling_version = int( modeling_versions[viol_label][i] )
+				if self.sub_version == 0:
+					modeling_version = int( self.modeling_versions[i] )
+				else:
+					modeling_version = float( self.modeling_versions[i] )
+
 				print( f"\n\033[1mModeling version = {modeling_version}" +
 						f"\t xl_weight = {xl_weight}\t" +
-						f"violation loss enabled = {enable_violation_loss} \033[0m\n" )
+						f"losses = {self.include_loss} \033[0m\n" )
 				self.run_modeling_benchmark(
 					modeling_version = modeling_version,
-					xl_weight = xl_weight,
-					enable_violation_loss = enable_violation_loss
+					xl_weight = xl_weight
 					)
+
+			self.plot_modeling_results()
 
 
 	def run_modeling_benchmark( self,
 		modeling_version: int,
-		xl_weight: float,
-		enable_violation_loss: bool ):
+		xl_weight: float ):
 		"""
 		Instantiate the BenchmarkModeling module and run the simulations.
 		"""
-		objective = "Rigid prior experiments. No FAPE, supervised_chi and CCOM loss. " + \
-					f"Using xl_weight = {xl_weight} and violation loss = {enable_violation_loss}"
+		objective = f"Rigid prior experiments. Using xl_weight = {xl_weight} and losses = {self.include_loss}"
 		sim = BenchmarkModeling()
 		sim.benchmark_name = self.benchmark_name
 		sim.modeling_version = modeling_version
-		sim.objective = objective
+		sim.modeling_objective = objective
 		sim.sys_conf_suff = self.sys_conf_suff
 		sim.device = "cuda:0"
 		sim.enable_relax_validate = False
 		sim.skip_rerun = True
 		sim.remove_sys_modeling_dir = self.remove_sys_modeling_dir
-		sim.violation["add_penalty"] = enable_violation_loss
+		sim.xlr["add_penalty"] = True
 		sim.xlr["weight"] = xl_weight
+		sim.create_summary_plots_and_files = False
+
+		self.enable_weights( obj = sim )
 
 		sim.forward()
 
+
+	def enable_weights( self, obj: BenchmarkModeling ):
+		"""
+		Enable the required weights as specified in self.include_loss.
+		Using shorthands for the different loss combinations.
+			v -> violations loss.
+			c -> ccom loss.
+			f -> full FAPE (backbone+interface+sidechain) loss
+			i -> only backbone+sidechain FAPE loss (intrafape)
+			s -> supervised_chi loss
+		e.g. vis -> violation + intrafape + supervised_chi loss
+		"""
+		if "v" in self.include_loss:
+			obj.violation["add_penalty"] = True
+		else:
+			obj.violation["add_penalty"] = False
+
+		if "c" in self.include_loss:
+			obj.ccom["add_penalty"] = True
+		else:
+			obj.ccom["add_penalty"] = False
+
+		if "f" in self.include_loss:
+			obj.fape["add_penalty"] = True
+		else:
+			obj.fape["add_penalty"] = False
+
+		if "i" in self.include_loss:
+			obj.fape["add_penalty"] = True
+			obj.fape["interfape"] = True
+		else:
+			obj.fape["interfape"] = False
+
+		if "s" in self.include_loss:
+			obj.supervised_chi["add_penalty"] = True
+		else:
+			obj.supervised_chi["add_penalty"] = False
+
 	################################################################################
 	################################################################################
-	def plot_modeling_results( self,  modeling_versions: List ):
+	def plot_modeling_results( self ):
 		"""
 		Create the following plots:
 			Per-epoch loss and xl restraint.
@@ -147,28 +218,26 @@ class RigigPrior():
 			RMSF wrt the average model.
 		"""
 		print( "\n" + "-"*70 + "\n" + "-"*70 + "\n\033[1mRigid prior plots\033[0m\n" )
-		for viol_label in modeling_versions:
-			for sys_name in self.benchmark["PDB ID"]:
-				print( f"Creating per-epoch loss plots for {sys_name}..." )
-				self.per_epoch_loss_plots(
-					sys_name = sys_name,
-					viol_label = viol_label,
-					modeling_versions = modeling_versions
-					)
+		for sys_name in self.benchmark["PDB ID"]:
+			sys_plot_dir = os.path.join( self.plots_dir, sys_name )
+			os.makedirs( sys_plot_dir, exist_ok = True )
+			print( f"Creating per-epoch loss plots for {sys_name}..." )
+			self.per_epoch_loss_plots(
+				sys_name = sys_name,
+				sys_plot_dir = sys_plot_dir
+				)
 
-				print( f"Creating RMSD plots for {sys_name}..." )
-				self.plot_rmsd_across_runs(
-					sys_name = sys_name,
-					viol_label = viol_label,
-					modeling_versions = modeling_versions
-					)
+			print( f"Creating RMSD plots for {sys_name}..." )
+			self.plot_rmsd_across_runs(
+				sys_name = sys_name,
+				sys_plot_dir = sys_plot_dir
+				)
 
-				print( f"Creating RMSF plots for {sys_name}..." )
-				self.plot_rmsf(
-					sys_name = sys_name,
-					viol_label = viol_label,
-					modeling_versions = modeling_versions
-					)
+			print( f"Creating RMSF plots for {sys_name}..." )
+			self.plot_rmsf(
+				sys_name = sys_name,
+				sys_plot_dir = sys_plot_dir
+				)
 
 
 	def get_input_for_per_epoch_plots( self,
@@ -192,7 +261,7 @@ class RigigPrior():
 		return violation, xlr, epochs
 
 
-	def per_epoch_loss_plots( self, sys_name: str, viol_label: str, modeling_versions: np.array ):
+	def per_epoch_loss_plots( self, sys_name: str, sys_plot_dir: str ):
 		"""
 		Plot the per-epoch xl restraint and violation loss across all runs.
 		"""
@@ -200,40 +269,44 @@ class RigigPrior():
 		_, ax = plt.subplots( 2, 1, figsize = ( 30, 20 ) )
 
 		for i, xl_weight in enumerate( self.xl_restraint_weights ):
-			modeling_version = modeling_versions[viol_label][i]
+			modeling_version = self.modeling_versions[i]
 			violation, xlr, epochs = self.get_input_for_per_epoch_plots(
 				sys_name = sys_name,
 				modeling_version = modeling_version )
 
 			ax[0].plot( epochs, violation, label = xl_weight )
-			ax[0].set_xlabel( "No. of epochs", fontsize = 25 )
-			ax[0].set_ylabel( "Violation loss", fontsize = 25 )
+			ax[0].set_xlabel( "No. of epochs", fontsize = 20 )
+			ax[0].set_ylabel( "Violation loss", fontsize = 20 )
 			ax[0].tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			ax[0].legend()
+			ax[0].legend( fontsize = 14 )
+
 			ax[1].plot( epochs, xlr, label = xl_weight )
 			ax[1].set_xlabel( "No. of epochs", fontsize = 20 )
 			ax[1].set_ylabel( "XL restraint", fontsize = 20 )
 			ax[1].tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			ax[1].legend()
+			ax[1].legend( fontsize = 14 )
 
-		benchmark_results_dir = get_benchmark_results_dir_path(
-			base_dir = "./benchmark",
-			benchmark_name = self.benchmark_name,
-			modeling_version = modeling_version )
+		ax[0].set_xlim( -0.1 )
+		ax[0].set_ylim( -0.1 )
+		ax[1].set_xlim( -0.1 )
+		ax[1].set_ylim( -0.1 )
 		loss_plot_file = os.path.join(
-			benchmark_results_dir, f"loss_per_epoch_{sys_name}_{viol_label}.png" )
-		plt.savefig( loss_plot_file, dpi = 300 )
+			sys_plot_dir, f"loss_per_epoch_{sys_name}_{self.include_loss}.png" )
+		plt.savefig( loss_plot_file, dpi = 300, bbox_inches = "tight" , pad_inches = 0.1 )
 		plt.close()
 
 	################################################################################
-	def plot_rmsd_across_runs( self,  sys_name: str, viol_label: str, modeling_versions: np.array ):
+	def plot_rmsd_across_runs( self,  sys_name: str, sys_plot_dir: str ):
 		"""
 		Across all runs, plot the RMSD for all models wrt the 1st model.
 		"""
 		plt.rcParams["font.family"] = "sans"
+		# cmap = plt.get_cmap( "RdYlGn_r" )
+		# norm = plt.Normalize( min( self.xl_restraint_weights ), max( self.xl_restraint_weights ) )
 		_, ax = plt.subplots( 1, 1, figsize = ( 30, 20 ) )
+
 		for i, xl_weight in enumerate( self.xl_restraint_weights ):
-			modeling_version = modeling_versions[viol_label][i]
+			modeling_version = self.modeling_versions[i]
 			ensemble_file = get_unrelaxed_ensemble_file(
 				base_dir = self.base_dir,
 				modeling_dir_name = self.benchmark_name,
@@ -253,19 +326,18 @@ class RigigPrior():
 			rmsd = compute_rmsd_post_align( ensemble_file = ensemble_file )
 			rmsd = rmsd[:,2]
 
+			# color = cmap( norm( xl_weight ) )
 			ax.plot( epochs, rmsd, label = xl_weight )
 			ax.set_xlabel( "No. of epochs", fontsize = 25 )
 			ax.set_ylabel( "RMSD wrt first model", fontsize = 25 )
 			ax.tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			ax.legend()
+			ax.legend( fontsize = 14 )
 
-		benchmark_results_dir = get_benchmark_results_dir_path(
-			base_dir = "./benchmark",
-			benchmark_name = self.benchmark_name,
-			modeling_version = modeling_version )
-		loss_plot_file = os.path.join(
-			benchmark_results_dir, f"rmsd_plots_{sys_name}_{viol_label}.png" )
-		plt.savefig( loss_plot_file, dpi = 300 )
+		ax.set_xlim( -0.1 )
+		ax.set_ylim( -0.1 )
+		rmsd_plot_file = os.path.join(
+			sys_plot_dir, f"rmsd_plots_{sys_name}_{self.include_loss}.png" )
+		plt.savefig( rmsd_plot_file, dpi = 300, bbox_inches = "tight" , pad_inches = 0.1 )
 		plt.close()
 
 	################################################################################
@@ -276,7 +348,7 @@ class RigigPrior():
 			Get pLDDT of initial predicted structure
 		"""
 		rmsf_avg = compute_rmsf_wrt_avg_model( ensemble_file = ensemble_file )
-		rmsf_first = compute_rmsf_wrt_first_model( ensemble_file = ensemble_file )
+		# rmsf_first = compute_rmsf_wrt_first_model( ensemble_file = ensemble_file )
 		resids = get_residue_ids( ensemble_file )
 
 		data_dir = get_sys_data_dir_path(
@@ -292,10 +364,10 @@ class RigigPrior():
 			out_dict = pkl.load( f )
 		plddt = out_dict["plddt"]
 
-		return rmsf_avg, rmsf_first, plddt, resids
+		return rmsf_avg, plddt, resids
 
 
-	def plot_rmsf( self, sys_name: str, viol_label: str, modeling_versions: np.array ):
+	def plot_rmsf( self, sys_name: str, sys_plot_dir: str ):
 		"""
 		Across all runs,
 			Plot RMSF wrt average model (this is essentially per-residue model precision)
@@ -304,7 +376,7 @@ class RigigPrior():
 		plt.rcParams["font.family"] = "sans"
 		_, ax = plt.subplots( len( self.xl_restraint_weights ), 2, figsize = ( 20, 40 ) )
 		for i, xl_weight in enumerate( self.xl_restraint_weights ):
-			modeling_version = modeling_versions[viol_label][i]
+			modeling_version = self.modeling_versions[i]
 
 			ensemble_file = get_unrelaxed_ensemble_file(
 				base_dir = self.base_dir,
@@ -314,52 +386,40 @@ class RigigPrior():
 				struct_format = self.struct_format
 				)
 
-			rmsf_avg, rmsf_first, plddt, resids = self.get_input_for_rmsf_plots(
+			rmsf_avg, plddt, resids = self.get_input_for_rmsf_plots(
 				sys_name = sys_name,
 				ensemble_file = ensemble_file )
 			# resids start from 1 for all chains. So, using system indices.
 			sys_idx = np.arange( 0, resids.shape[0], 1 )
 
-			corr, _ = pearsonr( rmsf_avg, plddt )
-			corr = np.round( corr, 3 )
+			corrs, _ = spearmanr( rmsf_avg, plddt )
+			corrs = np.round( corrs, 3 )
 			# RMSF wrt avg model per run.
 			ax[i, 0].plot( sys_idx, rmsf_avg, label = "RMSF" )
 			ax[i, 0].plot( sys_idx, plddt, label = "pLDDT" )
-			ax[i, 0].set_title( f"XL weight = {xl_weight}; Pearson corr = {corr}", fontsize = 20 )
+			ax[i, 0].set_title( f"XL weight = {xl_weight}; Spearman = {corrs}", fontsize = 20 )
 			ax[i, 0].set_xlabel( "Residue numbers", fontsize = 20 )
 			ax[i, 0].set_ylabel( "RMSF wrt average model", fontsize = 20 )
+			ax[i, 0].set_xlim( -0.5 )
+			ax[i, 0].set_ylim( -0.5 )
 			ax[i, 0].tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			ax[i, 0].legend()
+			ax[i, 0].legend( fontsize = 14 )
 
-			# corr, _ = scatter( rmsf_avg, plddt )
-			# corr = np.round( corr, 3 )
 			# RMSF wrt avg model per run.
-			ax[i, 1].scatter( plddt, rmsf_avg, label = "RMSF" )
-			# ax[i, 1].plot( sys_idx, plddt, label = "pLDDT" )
-			ax[i, 1].set_title( f"XL weight = {xl_weight}; Pearson corr = {corr}", fontsize = 20 )
+			ax[i, 1].scatter( plddt, rmsf_avg )
+			ax[i, 1].set_title( f"XL weight = {xl_weight}; Spearman = {corrs}", fontsize = 20 )
 			ax[i, 1].set_xlabel( "pLDDT", fontsize = 20 )
 			ax[i, 1].set_ylabel( "RMSF wrt average model", fontsize = 20 )
+			ax[i, 1].set_xlim( -0.5 )
+			ax[i, 1].set_ylim( -0.5 )
 			ax[i, 1].tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			ax[i, 1].legend()
 
-			# # RMSF vs pLDDT.
-			# ax[i, 1].scatter( rmsf, plddt, label = xl_weight )
-			# ax[i, 1].set_xlabel( "RMSF wrt average model", fontsize = 20 )
-			# ax[i, 1].set_ylabel( "pLDDT", fontsize = 20 )
-			# # ax[i, 0].set_xticks( sys_idx, resids )
-			# ax[i, 1].tick_params( axis = "both" , labelsize = 20, length = 10, width = 4 )
-			# ax[i, 1].legend()
 		plt.subplots_adjust( hspace = 0.5, wspace = 0.4 )
 
-		benchmark_results_dir = get_benchmark_results_dir_path(
-			base_dir = "./benchmark",
-			benchmark_name = self.benchmark_name,
-			modeling_version = modeling_version )
-		loss_plot_file = os.path.join(
-			benchmark_results_dir, f"rmsf_plots_{sys_name}_{viol_label}.png" )
-		plt.savefig( loss_plot_file, dpi = 300 )
+		rmsf_plot_file = os.path.join(
+			sys_plot_dir, f"rmsf_plots_{sys_name}_{self.include_loss}.png" )
+		plt.savefig( rmsf_plot_file, dpi = 300, bbox_inches = "tight" , pad_inches = 0.1 )
 		plt.close()
-
 
 	################################################################################
 	################################################################################
