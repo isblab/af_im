d48bb5fd7d76d797740659edb1299fe6c9a632bd
Kenobi98 <majilakartik123@gmail.com>
2025-08-25 18:38:54 +0530

    single rep modification

--- model.py
+++ model.py
@@ -32,6 +32,10 @@ def get_model( model_config: mlc.ConfigDict, system_features: mlc.ConfigDict,
 		print( f"Using PairPerturbation with adapter = {model_config.adapter.name}" )
 		model = PairPerturbation( system_features, ofold_config, model_config,
 											mode, is_multimer, device )
+	elif model_config.name == "single_perturbation":
+		print( f"Using SinglePerturbation with adapter = {model_config.adapter.name}" )
+		model = SinglePerturbation( system_features, ofold_config, model_config,
+											mode, is_multimer, device )
 	else:
 		raise ValueError( "Incorrect model type specified..." )
 
@@ -273,11 +277,11 @@ class LinearPerturbation( nn.Module ):
 		nn.init.zeros_( self.linear.bias )
 
 
-	def forward( self, z: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
-		z_mod = self.linear( self.lnorm( z ) )*self.alpha
+	def forward( self, rep: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
+		rep_mod = self.linear( self.lnorm( rep ) )*self.alpha
 		if inter_chain_mask is not None:
-			z_mod = z_mod*inter_chain_mask
-		return z + z_mod
+			rep_mod = rep_mod*inter_chain_mask
+		return rep + rep_mod
 
 
 class GatedLinearUnit( nn.Module ):
@@ -307,11 +311,11 @@ class GatedLinearUnit( nn.Module ):
 		self.alpha = alpha
 
 
-	def forward( self, z: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
-		z_mod = self.linear1( z ) * self.gate( self.linear2( z ) )*self.alpha
+	def forward( self, rep: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
+		rep_mod = self.linear1( rep ) * self.gate( self.linear2( rep ) )*self.alpha
 		if inter_chain_mask is not None:
-			z_mod = z_mod*inter_chain_mask
-		return z + self.alpha*z_mod
+			rep_mod = z_mod*inter_chain_mask
+		return rep + self.alpha*rep_mod
 
 
 class LoRA( nn.Module ):
@@ -327,11 +331,11 @@ class LoRA( nn.Module ):
 		self.lnorm = nn.LayerNorm( c_z, device = device )
 
 
-	def forward( self, z: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
-		z_mod = self.V( self.U( self.lnorm( z ) ) )*self.alpha
+	def forward( self, rep: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
+		rep_mod = self.V( self.U( self.lnorm( rep ) ) )*self.alpha
 		if inter_chain_mask is not None:
-			z_mod = z_mod*inter_chain_mask
-		return z + z_mod
+			rep_mod = rep_mod*inter_chain_mask
+		return rep + rep_mod
 
 
 class FiLM( nn.Module ):
@@ -354,17 +358,52 @@ class FiLM( nn.Module ):
 		self.lnorm = nn.LayerNorm( c_z, device = device )
 
 
-	def forward( self, z: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
+	def forward( self, rep: torch.Tensor, inter_chain_mask: torch.Tensor = None ):
 		gamma = self.gamma.view( 1, 1, 1, -1 )
 		beta = self.beta.view( 1, 1, 1, -1 )
 
-		y = self.lnorm( z )
-		z_mod = y*gamma + beta
+		y = self.lnorm( rep )
+		rep_mod = y*gamma + beta
 		if inter_chain_mask is not None:
-			z_mod = z_mod*inter_chain_mask
-		return z + z_mod
+			rep_mod = rep_mod*inter_chain_mask
+		return rep + rep_mod
 
 
+def get_adapter( c_z: int, config: mlc.ConfigDict, device: str ):
+	"""
+	Return the required adapter module.
+	Initialize the weights and biases to 0.
+	"""
+	adapter_name = config.adapter.name
+	if adapter_name == "linear_perturb":
+		adapter = LinearPerturbation(
+			c_z = c_z,
+			alpha = config.adapter.alpha,
+			device = device )
+	elif "gating" in adapter_name:
+		gate = adapter_name.split( "_" )[0]
+		adapter = GatedLinearUnit(
+			c_z = c_z,
+			gate = gate,
+			alpha = config.adapter.alpha,
+			device = device )
+	elif adapter_name == "lora":
+		adapter = LoRA(
+			c_z = c_z,
+			lora_k = config.adapter.lora_k,
+			alpha = config.adapter.alpha,
+			device = device )
+	elif adapter_name == "film":
+		adapter = FiLM(
+			c_z = c_z,
+			device = device )
+	else:
+		raise ValueError( f"Incorrect adapter specified: {adapter_name}..." )
+
+	return adapter
+
+################################################################################
+################################################################################
 class PairPerturbation( LoadState, Model ):
 	"""
 	Compose the AF2 structure module and pLDDT head.
@@ -414,7 +453,10 @@ class PairPerturbation( LoadState, Model ):
 			raise ValueError( "Incorrect mode: " +
 							f"{self.model_config.mode.lddt} for lddt head..." )
 
-		self.adapter = self.get_adapter()
+		self.adapter = get_adapter(
+			c_z = self.c_z,
+			config = self.model_config,
+			device = self.device )
 
 
 	def no_grad_for_sm( self ):
@@ -426,38 +468,38 @@ class PairPerturbation( LoadState, Model ):
 			p.requires_grad_( False )
 
 
-	def get_adapter( self ):
-		"""
-		Return the required adapter module.
-		Initialize the weights and biases to 0.
-		"""
-		adapter_name = self.model_config.adapter.name
-		if adapter_name == "linear_perturb":
-			adapter = LinearPerturbation(
-				c_z = self.c_z,
-				alpha = self.model_config.adapter.alpha,
-				device = self.device )
-		elif "gating" in adapter_name:
-			gate = adapter_name.split( "_" )[0]
-			adapter = GatedLinearUnit(
-				c_z = self.c_z,
-				gate = gate,
-				alpha = self.model_config.adapter.alpha,
-				device = self.device )
-		elif adapter_name == "lora":
-			adapter = LoRA(
-				c_z = self.c_z,
-				lora_k = self.model_config.adapter.lora_k,
-				alpha = self.model_config.adapter.alpha,
-				device = self.device )
-		elif adapter_name == "film":
-			adapter = FiLM(
-				c_z = self.c_z,
-				device = self.device )
-		else:
-			raise ValueError( f"Incorrect adapter specified: {adapter_name}..." )
+	# def get_adapter( self ):
+	# 	"""
+	# 	Return the required adapter module.
+	# 	Initialize the weights and biases to 0.
+	# 	"""
+	# 	adapter_name = self.model_config.adapter.name
+	# 	if adapter_name == "linear_perturb":
+	# 		adapter = LinearPerturbation(
+	# 			c_z = self.c_z,
+	# 			alpha = self.model_config.adapter.alpha,
+	# 			device = self.device )
+	# 	elif "gating" in adapter_name:
+	# 		gate = adapter_name.split( "_" )[0]
+	# 		adapter = GatedLinearUnit(
+	# 			c_z = self.c_z,
+	# 			gate = gate,
+	# 			alpha = self.model_config.adapter.alpha,
+	# 			device = self.device )
+	# 	elif adapter_name == "lora":
+	# 		adapter = LoRA(
+	# 			c_z = self.c_z,
+	# 			lora_k = self.model_config.adapter.lora_k,
+	# 			alpha = self.model_config.adapter.alpha,
+	# 			device = self.device )
+	# 	elif adapter_name == "film":
+	# 		adapter = FiLM(
+	# 			c_z = self.c_z,
+	# 			device = self.device )
+	# 	else:
+	# 		raise ValueError( f"Incorrect adapter specified: {adapter_name}..." )
 
-		return adapter
+	# 	return adapter
 
 
 	def get_inter_chain_mask( self, asym_id: torch.Tensor ):
@@ -486,6 +528,133 @@ class PairPerturbation( LoadState, Model ):
 		z = evo_output.get( "pair" )
 		inter_chain_mask = self.get_inter_chain_mask( asym_id = batch.get( "asym_id" ) )
 		evo_output["pair"] = self.adapter( z = z, inter_chain_mask = inter_chain_mask )
+
+		# Don't need the full Evoformer dict, just the Pair and Single representation.
+		outputs["sm"] = self.structure_module.forward(
+			evoformer_output_dict = evo_output,
+			aatype = gt_features["aatype"],
+			mask = self.system_features["seq_mask"].to(
+				dtype = evo_output["single"].dtype ) )
+
+		# The  dim=0 in all structure module outputs represents the no. of
+		# 	structure module blocks (default = 8).
+		outputs["final_atom_positions"] = atom14_to_atom37(
+													outputs["sm"]["positions"][-1],
+													gt_features
+													)
+		outputs["final_atom_mask"] = gt_features["atom37_atom_exists"]
+		outputs["final_affine_tensor"] = outputs["sm"]["frames"][-1]
+
+		with torch.no_grad():
+			# The AuxillaryHeads module requires pair, Single representations in the output dict.
+			# 	Even though not using the full AuxillaryHeads module, but still having this step.
+			outputs.update( evo_output )
+			# outputs.update( self.aux_heads( outputs ) )
+			lddt_logits = self.plddt( outputs["sm"]["single"] )
+			# Required for saving the structure later on.
+			outputs["plddt"] = compute_plddt( lddt_logits )
+
+		return outputs, batch
+
+
+	def params( self ):
+		"""
+		Return a list of models for the optimizer.
+		"""
+		return [self.adapter]
+
+
+################################################################################
+################################################################################
+class SinglePerturbation( LoadState, Model ):
+	"""
+	Compose the AF2 structure module and pLDDT head.
+	Modify the Single representation. The StructureModule can be kept frozen.
+	This class wraps multiple methods to modify the pair representation (z):
+		1. Linear perturbation
+		2. Gating
+		3. LoRA
+		4. FiLM
+	Further, we use a binary mask to ignore intra-chain elements in z.
+	"""
+	def __init__( self, system_features: mlc.ConfigDict,
+						ofold_config: mlc.ConfigDict,
+						model_config: mlc.ConfigDict,
+						mode: str, is_multimer: bool, device: str ):
+		LoadState.__init__( self, ofold_config, mode, is_multimer, device )
+		Model.__init__( self )
+
+		self.ofold_config = ofold_config
+		self.model_config = model_config
+		self.system_features = system_features
+		# Feature dim for pair representation.
+		self.c_z = 384
+
+		layers = ["structure_module", "lddt"]
+		self.load_pretrained_models( layers )
+
+		if self.model_config.mode.sm == "train":
+			print( "Using structure module in train mode" )
+			self.structure_module.train()
+		elif self.model_config.mode.sm == "eval":
+			print( "Using structure module in eval mode" )
+			self.structure_module.eval()
+		else:
+			raise ValueError( "Incorrect mode: " +
+							f"{self.model_config.mode.sm} for structure module..." )
+		if self.model_config.freeze_sm:
+			self.no_grad_for_sm()
+
+		if self.model_config.mode.plddt == "train":
+			print( "Using plddt head in train mode" )
+			self.plddt.train()
+		elif self.model_config.mode.plddt == "eval":
+			print( "Using plddt head in eval mode" )
+			self.plddt.eval()
+		else:
+			raise ValueError( "Incorrect mode: " +
+							f"{self.model_config.mode.lddt} for lddt head..." )
+
+		self.adapter = get_adapter(
+			c_z = self.c_z,
+			config = self.model_config,
+			device = self.device )
+
+
+	def no_grad_for_sm( self ):
+		"""
+		Turn OFF gradient computation for the StructureModule.
+		"""
+		print( "Switching OFF gradient computation for StructureModule." )
+		for p in self.structure_module.parameters():
+			p.requires_grad_( False )
+
+
+	def get_inter_chain_mask( self, asym_id: torch.Tensor ):
+		"""
+		Create a binary mask to ignore intra-chain interactions.
+		"""
+		if self.model_config.adapter.inter_mask:
+			if asym_id is not None:
+				# inter_chain_mask -> [N, N, 1]
+				inter_chain_mask = ( 
+					asym_id[..., None] != asym_id[..., None, :]
+					).to( self.device ).squeeze( 0 ). unsqueeze( -1 )
+		else:
+			inter_chain_mask = None
+		return inter_chain_mask
+
+
+	def predict( self, evo_output, gt_features, batch ):
+		"""
+		Run the structure module and auxillary heads (lddt) module.
+		If specified, mask out the intra-chain elements in pair_rep (z).
+		Apply the adapter to perturb the z.
+		"""
+		outputs = {}
+		# z -> [N, N, 128]
+		s = evo_output.get( "single" )
+		evo_output["single"] = self.adapter( rep = s, inter_chain_mask = None )
 
 		# Don't need the full Evoformer dict, just the Pair and Single representation.
 		outputs["sm"] = self.structure_module.forward(
