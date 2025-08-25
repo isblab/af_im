58d7a66f4d40f086b859c07b6c05a01c96cd0075
Kenobi98 <majilakartik123@gmail.com>
2025-08-25 18:38:33 +0530

    minor modifs

--- ensemble_to_gif.sh
+++ ensemble_to_gif.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 for sys_name in "4rhz" "8wtd"; do
-	sys_dir="/data2/kartik/IMP_Rewired/imp_dl/benchmark/rigid_modeling/$sys_name/version_"
+	sys_dir="/data2/kartik/IMP_Rewired/imp_dl/benchmark/pairrep_modeling/$sys_name/version_"
 	tmpdir="./tmp"
-	for sv in $(seq 0 0.1 0.5); do
-		# if [ "$sv" -eq 0 ]; then
-		# # if (( $(echo "$sv == 0" | bc -l) )); then
-		# 	s=$((1+$sv))
-		# 	e=$((10+$sv))
-		# else
-		s=$(echo "1+$sv"| bc)
-		e=$(echo "10+$sv"| bc)
-	    # Remove trailing 0 after decimal.
-	    s=${s%\.0}
-	    e=${e%\.0}
-		# fi
-		for i in $(seq $s 1 $e); do
-			echo "$sys_name $sv $s $e $i"
+	for v in $(seq 0 1 3); do
+	# for sv in $(seq 0 1 4); do
+		s=$(echo "$v+0"| bc)
+		e=$(echo "$v+0.4"| bc)
+		for i in $(seq $s 0.1 $e); do
+			# Remove trailing 0 after decimal.
+			i=${i%\.0}
+			echo "$sys_name $v $s $e $i"
 			pdbfile="${sys_dir}${i}/${sys_name}_output_models.pdb"
 			outfile="${sys_dir}${i}/${sys_name}_v${i}.gif"
 			vmd -dispdev text -e ./utils/vmd_visualize_traj.tcl -args "$pdbfile" "$outfile" "$tmpdir" 0.9 10
-		done
+			done
 	done
 done
