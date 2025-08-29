515890f0f8ab68f2c6f5fcdd54e5e99f3e5758bc
Kenobi98 <majilakartik123@gmail.com>
2025-08-29 17:54:30 +0530

    minor

--- ensemble_to_gif.sh
+++ ensemble_to_gif.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 for sys_name in "4rhz" "8wtd"; do
-	sys_dir="/data2/kartik/IMP_Rewired/imp_dl/benchmark/pairrep_modeling/$sys_name/version_"
+	sys_dir="/data2/kartik/IMP_Rewired/imp_dl/benchmark/rigidchain_modeling/$sys_name/version_"
 	tmpdir="./tmp"
-	for v in $(seq 0 1 3); do
-	# for sv in $(seq 0 1 4); do
+	# for i in $(seq 0 1 2); do
+	# 	# Remove trailing 0 after decimal.
+	# 	i=${i%\.0}
+	# 	echo "$sys_name $v $s $e $i"
+	# 	pdbfile="${sys_dir}${i}/${sys_name}_output_models.pdb"
+	# 	outfile="${sys_dir}${i}/${sys_name}_v${i}.gif"
+	# 	vmd -dispdev text -e ./utils/vmd_visualize_traj.tcl -args "$pdbfile" "$outfile" "$tmpdir" 0.9 10
+	# done
+
+	for v in $(seq 0 1 2); do
 		s=$(echo "$v+0"| bc)
-		e=$(echo "$v+0.4"| bc)
+		e=$(echo "$v+0.6"| bc)
 		for i in $(seq $s 0.1 $e); do
 			# Remove trailing 0 after decimal.
 			i=${i%\.0}
