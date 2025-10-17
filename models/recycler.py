fc3ef3cdc8aa4c0198ebc127abe40450871b1456
kenobi98 <majilakartik123@gmail.com>
2025-10-17 09:22:21 +0530

    recycler model

--- models/recycler.py
+++ models/recycler.py
@@ -13,28 +13,21 @@ from openfold.utils.import_weights import (
     import_jax_weights_ )
 from openfold.utils.script_utils import get_model_basename
 
-from loader import LoadState
-from base_model import Model
-from models.rigid_sampler import PoseSampling
+from openfold.utils.tensor_utils import (
+    tensor_tree_map )
 
-class TheForge( LoadState, Model ):
+
+class Recycler():
 	"""
-	Given a set of rigid bodies, do the following:
-		1. Predict rigid transformations to sample new configurations.
-		2. Use the predicted configuration (template) to bias the AF2 prediction.
-			> To let AF2 feel the effect of the new template, use AF sampling techniques.
+	Use the predicted configuration (template) to bias the AF2 prediction.
+		To let AF2 feel the effect of the new template, use AF sampling techniques.
 	"""
-	def __init__( self, feats: mlc.ConfigDict,
-						ofold_config: mlc.ConfigDict,
-						model_config: mlc.ConfigDict,
-						jax_param_path: str,
-						mode: str, is_multimer: bool, device: str ):
-		LoadState.__init__( self, ofold_config, mode, is_multimer, device )
-		Model.__init__( self )
+	def __init__( self,
+			ofold_config: mlc.ConfigDict,
+			jax_param_path: str,
+			 device: str ):
 		self.ofold_config = ofold_config
-		self.model_config = model_config
-		self.feats = feats
-
+		self.device = device
 
 		for path in jax_param_path.split( "," ):
 			model_basename = get_model_basename(path)
@@ -43,50 +36,57 @@ class TheForge( LoadState, Model ):
 			self.alphafold = self.alphafold.eval()
 			import_jax_weights_(
 				self.alphafold, path, version = model_version )
+
+
+	def forward( self,
+			out: Dict[str, Any],
+			batch: Dict[str, Any] ):
+		"""
+		"""
+		# Initialize recycling embeddings
+		m_1_prev = out["msa"][..., 0, :, :]
+		z_prev, x_prev = out["pair"], out["final_atom_positions"]
+		prevs = [m_1_prev, z_prev, x_prev]
+		del out
+
+		is_grad_enabled = torch.is_grad_enabled()
+
+		# Main recycling loop
+		num_iters = 1 # batch["aatype"].shape[-1]
+		#early_stop = False
+		#num_recycles = 0
+		#for cycle_no in range(num_iters):
+		# Select the features for the current recycling cycle.
+		fetch_cur_batch = lambda t: t[..., 0]
+		feats = tensor_tree_map(fetch_cur_batch, batch)
+		del batch
+
+		# Enable grad iff we're training and it's the final recycling layer
+		is_final_iter = True
+		with torch.set_grad_enabled(is_grad_enabled and is_final_iter):
+			if is_final_iter:
+				# Sidestep AMP bug (PyTorch issue #65766)
+				if torch.is_autocast_enabled():
+					torch.clear_autocast_cache()
+
 			self.alphafold = self.alphafold.to( self.device )
+			# Run the next iteration of the model
+			outputs, _, _, _, _ = self.alphafold.iteration(
+				feats,
+				prevs )
+				#_recycle = ( num_iters > 1 ) ) # not used inside iteration()
 
+		outputs["num_recycles"] = torch.tensor( [1] )
+		#outputs["num_recycles"] = torch.tensor(num_recycles, device=feats["aatype"].device)
 
+		outputs["asym_id"] = feats["asym_id"]
+		#if "asym_id" in batch:
+		#	outputs["asym_id"] = feats["asym_id"]
 
-	def forward( self ):
-		"""
-		"""
+		# Run auxiliary heads
+		outputs.update( self.alphafold.aux_heads( outputs ) )
 
-	def predict( self, outputs: Dict[str, Any],
-				gt_features: Dict[str, Any],
-				batch: Dict[str, Any] ):
-		"""
-		Update the current positions by applying a Rigid Transformation.
-		"""
-		# [B, N, 37, 3]; B -> batch size = 1; N -> no. of residues.
-		final_atom_positions = outputs.pop( "final_atom_positions" )
+		self.alphafold = self.alphafold.to( "cpu" )
 
-		rigid_bodies, init_mean_coords = self.split_into_rigid_bodies(
-			final_atom_positions = final_atom_positions,
-			asym_id = outputs["asym_id"]
-			)
-
-		# [B, 4] and [B, 3]
-		quat, trans = self.rigid( init_mean_coords )
-
-		transformed_positions = []
-		for rb, q, t in zip( rigid_bodies, quat, trans ):
-			R = self.quat_to_rotmat( quat = q )
-
-			Rt_rb = self.apply_transform(
-				coords = rb,
-				R = R,
-				trans = t )
-			transformed_positions.append( Rt_rb )
-		transformed_positions = torch.cat( transformed_positions, dim = 1 )
-		outputs["final_atom_positions"] = transformed_positions*gt_features["atom37_atom_exists"].unsqueeze( -1 )
-		print( outputs["final_atom_positions"].shape )
-
-		return outputs, batch
-
-
-	def params( self ):
-		"""
-		Return a list of models for the optimizer.
-		"""
-		return [self.rigid]
+		return outputs
 
