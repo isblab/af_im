1164a97513cb7d2448c71612bb35dd7163ee7ca0
kenobi98 <majilakartik123@gmail.com>
2025-10-17 09:21:30 +0530

    contains func to obtain rigid bodies

--- models/rigid_body.py
+++ models/rigid_body.py
@@ -3,9 +3,28 @@ Given an AF2 prediction, define rigid bodies
 	Split chains into rigid bodies.
 	Based on pLDDT and/or PAE.
 """
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Any
 
 import torch
+
+ALLOWED_RIGID_TYPES = ["chains"]
+
+def get_rigid_body(
+	#out: Dict[str, Any],
+	final_atom_positions: torch.Tensor,
+	asym_id: torch.Tensor,
+	rigid_type: str):
+	"""
+	Return rigid bodies created as specified by rigid_type.
+	"""
+	if rigid_type == "chains":
+		return get_rigid_chains(
+			final_atom_positions = final_atom_positions,
+			asym_id = asym_id )
+	else:
+		raise ValueError( f"Incorrect rigid_type = '{rigid_type}' specified." +
+				   " Allowed [{ALLOWED_RIGID_TYPES}]..." )
+
 
 def get_rigid_chains(
 	final_atom_positions: torch.Tensor,
@@ -19,6 +38,7 @@ def get_rigid_chains(
 
 	final_atom_posistion -> [1, N, 37, 3]
 	"""
+	assert len( final_atom_positions.shape ) == 4, f"Incorrect shape for final_atom_positions {final_atom_positions.shape}"
 	rigid_bodies = []
 	unique_asym_ids = torch.unique( asym_id )
 
