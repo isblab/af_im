274e8ac560497a34a258a60f36220018f5b0c622
kenobi98 <majilakartik123@gmail.com>
2025-12-19 18:04:58 +0530

    added more path funcs

--- utils/paths.py
+++ utils/paths.py
@@ -1,21 +1,72 @@
 """
 Funstions to create paths for the system or benchmark directories and files.
+Each input complex being modeled is identified by the sys_name.
+This could be the PDB ID for all complexes in the benchmark.
+The following dir structure is followed in this framework:
+BASE_DIR/
+	*_benchmark/
+		For each input system, contains a dir containing the initial OpenFold
+			prediction, native struct file, data file (.csv file for XLs).
+	*_benchmark_analysis/
+		For each benchmark run, contains a dir containing the analysis results
+			and configs for the run.
+	*_metadata/
+		Contains the JWALK predicted XLs file, configs for creating the benchmark,
+			metrics for the initial OpenFold prediction, native structures for all
+			systems in the benchmark.
+	*_modeling/
+		For each system, contains a dir per simulation (modeling_version)
+			containing all outputs generated during the simulation.
 """
+from typing import Any
 import os
+
+# Dir to store all relevant modeling output.
+BASE_DIR = "./benchmark/"
+
+
+def get_meta_dir_path(
+	base_dir: str,
+	benchmark_name: str ) -> str:
+	"""
+	Return the path to the metadat dir for the benchmark.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+
+	Return:
+	----------
+	meta_dir_path -> path to the metadata dir.
+	"""
+	meta_dir = os.path.join(
+		base_dir, f"{benchmark_name}_metadata/" )
+	return meta_dir
 
 
 def get_sys_modeling_path(
 	base_dir: str,
 	modeling_dir_name: str, sys_name: str ) -> str:
 	"""
-	Return thr path to the modeling dir for given system.
+	Return the path to the modeling dir for given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	modeling_dir_name -> name of the modeling dir.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+
+	Return:
+	----------
+	sys_modeling_path -> path to the system-specific modeling dir.
 	"""
-	sys_path = os.path.join( 
+	sys_modeling_path = os.path.join( 
 				os.path.abspath(
 					f"{base_dir}/{modeling_dir_name}/{sys_name}"
 					)
 		)
-	return sys_path
+	return sys_modeling_path
 
 
 def get_sys_data_dir_path(
@@ -23,6 +74,17 @@ def get_sys_data_dir_path(
 	benchmark_name: str, sys_name: str ) -> str:
 	"""
 	Return the path to the system data directory.
+		data_dir -> BASE_DIR/*_benchmark/sys_name/
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+
+	Return:
+	----------
+	data_dir -> path to the system specific data dir.
 	"""
 	data_dir = os.path.join(
 		base_dir,
@@ -30,24 +92,69 @@ def get_sys_data_dir_path(
 	return data_dir
 
 
-def get_benchmark_results_dir_path(
+def get_benchmark_analysis_dir_path(
 	base_dir: str,
 	benchmark_name: str ) -> str:
 	"""
-	Return the path to the benchmark results dir.
+	Return the path to the benchmark analysis dir.
+		BASE_DIR/*_benchmark_analysis/
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+
+	Return:
+	----------
+	benchmark_analysis_dir -> path to the analysis dir for the benchmark.
 	"""
-	benchmark_results_dir = os.path.join(
+	benchmark_analysis_dir = os.path.join(
 		base_dir,
-		f"{benchmark_name}_benchmark_results" )
-	return benchmark_results_dir
+		f"{benchmark_name}_benchmark_analysis" )
+	return benchmark_analysis_dir
+
+
+def get_benchmark_analysis_version_dir_path(
+	base_dir: str,
+	benchmark_name: str,
+	modeling_version: Any ) -> str:
+	"""
+	Return the path to the benchmark analysis dir for
+		the given modleing version.
+		BASE_DIR/*_benchmark_analysis/version_{VERSION}/
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+
+	Return:
+	----------
+	benchmark_analysis_dir -> path to the analysis dir for the benchmark.
+	"""
+	benchmark_analysis_ver_dir = os.path.join(
+		base_dir,
+		f"{benchmark_name}_benchmark_analysis/version_{modeling_version}" )
+	return benchmark_analysis_ver_dir
 
 
 def get_sys_modeling_version_path(
 	base_dir: str,
 	modeling_dir_name: str,
-	sys_name: str, modeling_version: int ) -> str:
+	modeling_version: Any, sys_name: str ) -> str:
 	"""
 	Return the path to the system modeling version dir.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	modeling_version -> an identifier for the simulation version.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+
+	Return:
+	----------
+	ver_path -> path to the system-specific modleing version dir.
 	"""
 	sys_path = get_sys_modeling_path(
 		base_dir = base_dir,
@@ -61,10 +168,23 @@ def get_sys_modeling_version_path(
 
 def get_analysis_dir_path(
 	base_dir: str,
+	sys_name: str,
 	modeling_dir_name: str,
-	sys_name: str, modeling_version: int ) -> str:
+	modeling_version: Any ) -> str:
 	"""
 	Return the path to the analysis_dict file for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+
+	Return:
+	----------
+	analysis_dir_path -> path to the system-specific analysis dir for
+		the given modleing version.
 	"""
 	ver_path = get_sys_modeling_version_path(
 		base_dir = base_dir,
@@ -81,6 +201,17 @@ def get_sys_config_path(
 	sys_name: str, sys_conf_suff: str ) -> str:
 	"""
 	Return the path to the config dict for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	sys_conf_suff -> suffix for the system config file.
+
+	Return:
+	----------
+	sys_config_file -> path to the system-specific config file.
 	"""
 	data_dir = get_sys_data_dir_path(
 		base_dir = base_dir,
@@ -99,6 +230,17 @@ def get_xl_file_path(
 	sys_name: str, sys_conf_suff: str ) -> str:
 	"""
 	Return the path to the XLs .csv file for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	sys_conf_suff -> suffix for the system config file.
+
+	Return:
+	----------
+	xl_file_path -> path to the XLs .csv file.
 	"""
 	data_dir = get_sys_data_dir_path(
 		base_dir = base_dir,
@@ -111,32 +253,80 @@ def get_xl_file_path(
 	return xl_file_path
 
 
-def get_benchmark_modeling_version_path(
+def get_benchmark_analysis_version_path(
 	base_dir: str,
 	benchmark_name: str,
-	modeling_version: int ) -> str:
+	modeling_version: Any ) -> str:
 	"""
-	Return the path to the benchmark results dir.
+	Return the path to the benchmark analysis dir for the given modeling version.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	modeling_version -> an identifier for the simulation version.
+
+	Return:
+	----------
+	benchmark_modeling_version_path -> path to the analysis dir for the
+		benchmark for the given modeling version.
 	"""
-	benchmark_results_dir = get_benchmark_results_dir_path(
+	benchmark_analysis_dir = get_benchmark_analysis_dir_path(
 		base_dir = base_dir,
 		benchmark_name = benchmark_name,
 		modeling_version = modeling_version )
 
 	benchmark_modeling_version_path = os.path.join(
-		benchmark_results_dir,
+		benchmark_analysis_dir,
 		f"version_{modeling_version}")
 
 	return benchmark_modeling_version_path
 
 
+def get_benchmark_csv_file(
+	base_dir: str,
+	benchmark_name: str ) -> str:
+	"""
+	Return the path to the selected benchmark .csv file.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+
+	Return:
+	----------
+	stat_file_path -> path to the benchmark .csv file.
+	"""
+	meta_dir = get_meta_dir_path(
+		base_dir = base_dir,
+		benchmark_name = benchmark_name )
+	csv_file_path = os.path.join(
+		meta_dir, f"selected_{benchmark_name}_benchmark.csv"
+	)
+	return csv_file_path
+
+
 def get_stat_file_path(
 	base_dir: str,
+	sys_name: str,
 	modeling_dir_name: str,
-	sys_name: str, modeling_version: int ) -> str:
+	modeling_version: Any ) -> str:
 	"""
 	Return the path to the stats file for the given system.
 	For all systems, stats file is named Stats.npy.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+
+	Return:
+	----------
+	stat_file_path -> path to the system-specific stats file for the
+		given modeling version.
 	"""
 	ver_path = get_sys_modeling_version_path(
 		base_dir = base_dir,
@@ -149,10 +339,23 @@ def get_stat_file_path(
 
 def get_analysis_dict_path(
 	base_dir: str,
+	sys_name: str,
 	modeling_dir_name: str,
-	sys_name: str, modeling_version: int ) -> str:
+	modeling_version: Any ) -> str:
 	"""
 	Return the path to the analysis_dict file for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+
+	Return:
+	----------
+	analysis_dict_file -> path to the system-specific analysis file for the
+		given modeling version.
 	"""
 	analysis_dir_path = get_analysis_dir_path(
 		base_dir = base_dir,
@@ -165,11 +368,22 @@ def get_analysis_dict_path(
 
 def get_unrelaxed_model_file(
 	base_dir: str,
-	modeling_dir_name: str,
 	sys_name: str,
-	modeling_version: int, model_id: int ):
+	modeling_dir_name: str,
+	modeling_version: Any,
+	model_id: int,
+	struct_format: str = ".pdb" ):
 	"""
 	Get the file to the unrelaxed model (saved during fine-tuning).
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+	model_id -> an identifier for the model (predicted structure).
+	struct_format -> file format for the structure file (pdb/cif).
 	"""
 	ver_path = get_sys_modeling_version_path(
 		base_dir = base_dir,
@@ -179,18 +393,29 @@ def get_unrelaxed_model_file(
 	models_dir = os.path.join( 
 	ver_path, f"{sys_name}_ensemble")
 	model_file = os.path.join(
-		models_dir, f"model_{model_id}.pdb" )
+		models_dir, f"model_{model_id}.{struct_format}" )
 	return model_file
 
 
 def get_relaxed_model_file(
 	base_dir: str,
-	modeling_dir_name: str,
 	sys_name: str,
-	modeling_version: int, model_id: int ):
+	modeling_dir_name: str,
+	modeling_version: Any,
+	model_id: int,
+	struct_format: str = ".pdb" ):
 	"""
 	Get the file to the relaxed model (saved during fine-tuning).
 	Only good-scoring models are relaxed and saved.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+	model_id -> an identifier for the model (predicted structure).
+	struct_format -> file format for the structure file (pdb/cif).
 	"""
 	analysis_dir_path = get_analysis_dir_path(
 		base_dir = base_dir,
@@ -200,18 +425,26 @@ def get_relaxed_model_file(
 	models_dir = os.path.join( 
 	analysis_dir_path, f"relaxed_models" )
 	model_file = os.path.join(
-		models_dir, f"model_{model_id}.pdb" )
+		models_dir, f"model_{model_id}.{struct_format}" )
 	return model_file
 
 
 def get_unrelaxed_ensemble_file(
 	base_dir: str,
-	modeling_dir_name: str,
 	sys_name: str,
-	modeling_version: int,
+	modeling_dir_name: str,
+	modeling_version: Any,
 	struct_format: str ):
 	"""
 	Get the file to the unrelaxed model (saved during fine-tuning).
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+	struct_format -> file format for the structure file (pdb/cif).
 	"""
 	ver_path = get_sys_modeling_version_path(
 		base_dir = base_dir,
@@ -226,12 +459,20 @@ def get_unrelaxed_ensemble_file(
 
 def get_relaxed_ensemble_file(
 	base_dir: str,
-	modeling_dir_name: str,
 	sys_name: str,
-	modeling_version: int,
+	modeling_dir_name: str,
+	modeling_version: Any,
 	struct_format: str ):
 	"""
 	Get the file to the unrelaxed model (saved during fine-tuning).
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	modeling_dir_name -> name of the modeling dir.
+	modeling_version -> an identifier for the simulation version.
+	struct_format -> file format for the structure file (pdb/cif).
 	"""
 	analysis_dir_path = get_analysis_dir_path(
 		base_dir = base_dir,
@@ -251,6 +492,12 @@ def get_native_struct_file(
 ):
 	"""
 	Return the path to the native structure file for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
 	"""
 	data_dir = get_sys_data_dir_path(
 		base_dir = base_dir,
@@ -268,6 +515,12 @@ def get_init_struct_file(
 ):
 	"""
 	Return the path to the initial OpenFold predicted structure for the given system.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
 	"""
 	data_dir = get_sys_data_dir_path(
 		base_dir = base_dir,
@@ -278,3 +531,29 @@ def get_init_struct_file(
 		data_dir,
 		f"{sys_name}_output/predictions/{sys_name}_init_pred_relaxed.cif" )
 	return init_struct_file
+
+
+def get_init_pred_file(
+	base_dir: str,
+	benchmark_name: str,
+	sys_name: str
+):
+	"""
+	Return the path to the initial OpenFold prediction for
+		the given system stored as a .pkl file.
+
+	Input:
+	----------
+	base_dir -> dir to store all relevant modeling output.
+	benchmark_name -> name of the benchmark.
+	sys_name -> name of the complex modeled. For the benchmark, it's the PDB ID.
+	"""
+	data_dir = get_sys_data_dir_path(
+		base_dir = base_dir,
+		benchmark_name = benchmark_name,
+		sys_name = sys_name
+	)
+	init_pred_file = os.path.join(
+		data_dir,
+		f"{sys_name}_output/predictions/output_dict.pkl" )
+	return init_pred_file
