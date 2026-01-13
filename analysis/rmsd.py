29ceb5976c4794217ebc691e01153eb29800cf8f
kenobi98 <majilakartik123@gmail.com>
2026-01-13 18:39:44 +0530

    clean up

--- analysis/rmsd.py
+++ analysis/rmsd.py
@@ -146,12 +146,6 @@ class StructuralSimilarity():
 				if model_id1 not in selected_models:
 					selected_models.append( model_id1 )
 
-			# selected_model_index = np.where(
-			# 	np.isin(
-			# 		np.array( self.model_ids ), np.array( selected_models )
-			# 		)
-			# 	)
-
 		return np.array( selected_models )
 
 	################################################################################
@@ -162,10 +156,12 @@ class StructuralSimilarity():
 		Remove structurally similar models (RMSD <= similarity_cutoff).
 		Using USalign for computing RMSD.
 		"""
-		selected_model_index = []
+		metric = self.rmsd_config.metric
+
+		selected_models = []
 		ignore_models = []
 		if len( self.model_ids ) == 1:
-			selected_model_index = copy.copy( self.model_ids )
+			selected_models = copy.copy( self.model_ids )
 		else:
 			total_models = len( self.model_ids )
 			for i in range( total_models ):
@@ -179,22 +175,23 @@ class StructuralSimilarity():
 					model_id2 = self.model_ids[j]
 					if model_id1 == model_id2 or model_id2 in ignore_models:
 						continue
-					
+
 					rmsd, tm = self.get_tm_from_usalign(
 						model_id1 = model_id1,
 						model_id2 = model_id2 )
+					similarity = rmsd if metric == "rmsd" else tm
 
-					similarity = rmsd if self.rmsd_config.metric == "rmsd" else tm
-
-					if similarity <= self.rmsd_config.similarity_cutoff:
+					if metric == "rmsd" and similarity <= self.rmsd_config.similarity_cutoff:
+						ignore_models.append( model_id2 )
+					elif metric == "tm" and similarity >= self.rmsd_config.similarity_cutoff:
 						ignore_models.append( model_id2 )
 					else:
 						self.rmsd_dict[f"model_{model_id1}_{model_id2}"] = {
 							"rmsd": rmsd, "tm": tm}
 
-				if model_id1 not in selected_model_index:
-					selected_model_index.append( model_id1 )
-		return np.array( selected_model_index )
+				if model_id1 not in selected_models:
+					selected_models.append( model_id1 )
+		return np.array( selected_models )
 
 
 	def get_tm_from_usalign( self, model_id1: int, model_id2: int ):
@@ -214,62 +211,3 @@ class StructuralSimilarity():
 			)
 		rmsd, tm = get_alignment_score( stdout_file )
 		return rmsd, tm
-
-	# def usalign( self, model_id1: int, model_id2: int ):
-	# 	"""
-	# 	Use USalign for computing the TM-score
-	# 		and RMSD for the given models.
-	# 	Assuming model_id1 to be the reference.
-
-	# 	mol --> molecule type [auto, prot, RNA.
-	# 	mm --> multimeric laignment option.
-	# 		0: alignment of two monomeric structures.
-	# 		1: alignment of two multi-chain oligomeric structures.
-	# 		2: alignment of individual chains to an oligomeric structure.
-	# 		Look at USalign -h option for more details.
-	# 	ter --> #chains to align.
-	# 		0: align all chains from all models.
-	# 		1: align all chains of the first model.
-	# 		2: only align the first chain.
-
-	# 	USalign model1.pdb model2.pdb -ter 0 -mm 1 -mol prot
-	# 	"""
-	# 	model1 = self.get_struct_file( model_id = model_id1 )
-	# 	model2 = self.get_struct_file( model_id = model_id2 )
-
-	# 	stdout_file = os.path.join( self.tmp_dir, f"model_{model_id1}_{model_id2}.txt" )
-	# 	stderr_file = os.path.join( self.tmp_dir, f"error_{model_id1}_{model_id2}.txt" )
-
-	# 	cmd = [f"./{self.rmsd_config.usalign_script}", 
-	# 			f"{model1}",
-	# 			f"{model2}",
-	# 			"-mol", "prot",
-	# 			"-mm", f"{self.rmsd_config.mm}",
-	# 			"-ter", f"{self.rmsd_config.ter}"]
-
-	# 	run_subprocess(
-	# 		command = cmd,
-	# 		stdout_file = stdout_file,
-	# 		stderr_file = stderr_file
-	# 	)
-	# 	return stdout_file
-
-
-	# def get_alignment_score( self, stdout_file: str ):
-	# 	"""
-	# 	Return the TM-score and RMSD.
-	# 	Read the MMalign/USalign output stored in a txt file.
-	# 		Line14: Aligned length= 572, RMSD=   0.76, Seq_ID=n_identical/n_aligned= 1.000
-	# 		Line15: TM-score= 0.XXXXX (if normalized by length of Chain_1, i.e., LN=XX, d0=X.XX)
-	# 		Line16: TM-score= 0.XXXXX (if normalized by length of Chain_2, i.e., LN=XXX, d0=X.XX)
-	# 	"""
-	# 	with open( stdout_file, "r" ) as f:
-	# 		output = f.readlines()
-
-	# 	rmsd_line = output[14]
-	# 	tm_line = output[15]
-
-	# 	rmsd = float( rmsd_line.split( "," )[1].split( "RMSD=" )[1] )
-	# 	tm = float( tm_line.split( " " )[1] )
-	# 	return rmsd, tm
-
