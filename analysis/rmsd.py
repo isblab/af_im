6365f2f3f897125818c16198a0c4e5428d175ea3
kenobi98 <majilakartik123@gmail.com>
2026-05-01 15:37:05 +0530

    fixed running the pipeline

--- analysis/rmsd.py
+++ analysis/rmsd.py
@@ -23,16 +23,19 @@ class StructuralSimilarity():
 		model_ids2: List[int],
 		model_files2: List[str],
 		tmp_dir_path: str,
+		cpu_cores: int,
 		mol: str = "prot",
 		mm: int = 1,
 		ter: int = 1
 	):
+		self.usalign_script = "USalign"
 		self.model_ids1 = model_ids1
 		self.model_files1 = model_files1
 		self.model_ids2 = model_ids2
 		self.model_files2 = model_files2
 		self.tmp_dir_path = tmp_dir_path
 
+		self.cpu_cores = cpu_cores
 		# USalign parameters
 		self.mol = mol
 		self.mm = mm
@@ -48,19 +51,19 @@ class StructuralSimilarity():
 		self.struct_models_exist()
 		self.create_tmp_dir()
 
-		# Sanity check.
-		if self.rmsd_config.metric not in ["rmsd", "tm"]:
-			raise ValueError( "Incorrect structural similarity metric specified. Use rmsd/tm..." )
-		if self.rmsd_config.similarity_cutoff < 0.0:
-			raise ValueError( f"Cannot use negative similarity cutoff..." )
-		if self.rmsd_config.metric == "tm":
-			if self.rmsd_config.similarity_cutoff > 1.0:
-				raise ValueError( "Similarity cutoff for TM-score cannot be >1.0..." )
+		# # Sanity check.
+		# if self.rmsd_config.metric not in ["rmsd", "tm"]:
+		# 	raise ValueError( "Incorrect structural similarity metric specified. Use rmsd/tm..." )
+		# if self.rmsd_config.similarity_cutoff < 0.0:
+		# 	raise ValueError( f"Cannot use negative similarity cutoff..." )
+		# if self.rmsd_config.metric == "tm":
+		# 	if self.rmsd_config.similarity_cutoff > 1.0:
+		# 		raise ValueError( "Similarity cutoff for TM-score cannot be >1.0..." )
 
 		self.compute_structural_similarity_parallel()
 
-		if self.rmsd_config.clean_up:
-			self.remove_tmp_dir()
+		# if self.rmsd_config.clean_up:
+		self.remove_tmp_dir()
 		return self.similarity_dict
 
 
@@ -69,12 +72,12 @@ class StructuralSimilarity():
 		Create a temporary dir for storing intermediate
 			files for rmsd computation.
 		"""
-		self.tmp_dir = os.path.join( self.analysis_dir, "rmsd_tmp" )
-		os.makedirs( self.tmp_dir, exist_ok = True )
+		# self.tmp_dir = os.path.join( self.analysis_dir, "rmsd_tmp" )
+		os.makedirs( self.tmp_dir_path, exist_ok = True )
 
 
 	def remove_tmp_dir( self ):
-		cmd = ["rm", "-r", f"{self.tmp_dir}"]
+		cmd = ["rm", "-r", f"{self.tmp_dir_path}"]
 		run_subprocess( cmd )
 
 
@@ -83,12 +86,12 @@ class StructuralSimilarity():
 		Check if the predicted structure files exist on disk.
 		"""
 		for model_file in self.model_files1:
-			if not os.path.exists( self.model_file ):
+			if not os.path.exists( model_file ):
 				raise FileNotFoundError( 
 					f"Model1: {model_file} does not exist..."
 				 )
 		for model_file in self.model_files2:
-			if not os.path.exists( self.model_file ):
+			if not os.path.exists( model_file ):
 				raise FileNotFoundError( 
 					f"Model2: {model_file} does not exist..."
 				 )
@@ -108,27 +111,30 @@ class StructuralSimilarity():
 			}
 		}
 		"""
+		model1s = list( zip( self.model_ids1, self.model_files1 ) )
 		with Pool( self.cpu_cores ) as p:
 			for result in tqdm.tqdm(
 				p.imap_unordered(
-					self.compute_structural_similarity_sequentially(
-							zip( self.model_ids1, self.model_files1 )
-						),
+					self.compute_structural_similarity_sequentially,
+					model1s,
 					chunksize = self.cpu_cores//2
 				),
-				total = len( self.model_files1 )
+				total = len( model1s )
 			):
 				model_id1, sim_dict = result
 				self.similarity_dict[model_id1] = sim_dict
 
 
-	def compute_structural_similarity_sequentially( self, model1: Iterable[Tuple[int, str]] ):
+	def compute_structural_similarity_sequentially(
+		self,
+		model1: Tuple[int, str]
+		) -> Tuple[int, Dict[int, Dict]]:
 		"""
 		Compute the similarity of the model1 with all models in set 2 (model_ids2).
 		This is done sequentially.
 		"""
 		sim_dict = {}
-		model_id1, model_file1 = list( model1 )
+		model_id1, model_file1 = model1
 		for i, model_id2 in enumerate( self.model_ids2 ):
 			model_file2 = self.model_files2[i]
 			rmsd, tm = self.get_similarity_from_usalign(
@@ -155,12 +161,12 @@ class StructuralSimilarity():
 		Run USalign and return the RMSD and TM-score.
 		"""
 		stdout_file = usalign(
-			usalign_script = self.rmsd_config.usalign_script,
+			usalign_script = self.usalign_script,
 			model_id1 = model_id1,
 			model1_file = model_file1,
 			model_id2 = model_id2,
 			model2_file = model_file2,
-			tmp_dir = self.tmp_dir,
+			tmp_dir = self.tmp_dir_path,
 			mol = self.mol,
 			mm = self.mm,
 			ter = self.ter,
