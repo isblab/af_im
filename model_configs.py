06629ae1bc44c50703080b13656973b038c3ab57
kenobi98 <majilakartik123@gmail.com>
2026-05-12 13:50:21 +0530

    updated

--- model_configs.py
+++ model_configs.py
@@ -8,6 +8,7 @@ BOLTZ = mlc.ConfigDict(
 {
 	"alpha": {
 		"xl_type": None,          # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "unguided",  # guided/unguided
 		# If True, use MSA subsampling.
 		"subsample_msa": False,  # default = False
@@ -18,11 +19,15 @@ BOLTZ = mlc.ConfigDict(
 		# No. of samples to predict.
 		"diffusion_samples": 25,  # default = 25
 		# No. of diffusion sampling steps.
-		"sampling_steps": 200  # default = 200
-
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
 	},
-	"beta": {
+	"beta1": {
 		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",  # guided/unguided
 		# If True, use MSA subsampling.
 		"subsample_msa": False,  # default = False
@@ -33,10 +38,89 @@ BOLTZ = mlc.ConfigDict(
 		# No. of samples to predict.
 		"diffusion_samples": 25,  # default = 25
 		# No. of diffusion sampling steps.
-		"sampling_steps": 200  # default = 200
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
 	},
 	"gamma": {
 		"xl_type": "long",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": False,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 1024,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 3,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 25,  # default = 25
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200  # default = 200
+	},
+	"delta1": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": True,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 30,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 3,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 25,  # default = 25
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
+	},
+	"delta2": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": True,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 128,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 3,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 25,  # default = 25
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
+	},
+	"delta3": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": True,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 256,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 3,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 25,  # default = 25
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
+	},
+	"epsilon1": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",  # guided/unguided
 		# If True, use MSA subsampling.
 		"subsample_msa": False,  # default = False
@@ -47,8 +131,51 @@ BOLTZ = mlc.ConfigDict(
 		# No. of samples to predict.
 		"diffusion_samples": 25,  # default = 25
 		# No. of diffusion sampling steps.
-		"sampling_steps": 200  # default = 200
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.0   # default = 1.638
 	},
+	"zeta1": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": False,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 1024,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 1,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 25,  # default = 25
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 5,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
+	},
+	"eta1": {
+		"xl_type": "short",     # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
+		"pred_type": "guided",  # guided/unguided
+		# If True, use MSA subsampling.
+		"subsample_msa": False,  # default = False
+		# No. of MSA seq to subsample.
+		"num_subsampled_msa": 1024,  # default = 1024
+		# No. of recycling iterations.
+		"recycling_steps": 3,  # default = 3
+		# No. of samples to predict.
+		"diffusion_samples": 50,  # default = 25
+		# No. of diffusion sampling steps.
+		"sampling_steps": 200,  # default = 200
+		# max no. of samples to predict in parallel.
+		"max_parallel_samples": 1,  # default = 5
+		# Related to the temperature at which diffusion samples the distribution.
+		"step_scale": 1.638   # default = 1.638
+	},
+
 })
 
 
@@ -57,8 +184,9 @@ BOLTZ = mlc.ConfigDict(
 ALPHALINK = mlc.ConfigDict(
 {
 	"alpha": {},
-	"beta": {
+	"beta1": {
 		"xl_type": "short",          # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",  # guided/unguided
 		# Use templates before this date.
 		"max_template_date": "2020-05-01", # default
@@ -69,6 +197,7 @@ ALPHALINK = mlc.ConfigDict(
 	},
 	"gamma": {
 		"xl_type": "long",      # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",  # guided/unguided
 		# Use templates before this date.
 		"max_template_date": "2020-05-01", # default
@@ -86,14 +215,27 @@ GRASP = mlc.ConfigDict(
 {
 	"alpha": {
 		"xl_type": None,          # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "unguided",  # guided/unguided
 	},
-	"beta": {
+	"beta1": {
 		"xl_type": "short",      # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",   # guided/unguided
 	},
+	# "beta2": {
+	# 	"xl_type": "short",      # short/long/None
+	# 	"frac_fp": 0.25,   # fraction of FP XLs
+	# 	"pred_type": "guided",   # guided/unguided
+	# },
+	# "beta3": {
+	# 	"xl_type": "short",      # short/long/None
+	# 	"frac_fp": 0.5,   # fraction of FP XLs
+	# 	"pred_type": "guided",   # guided/unguided
+	# },
 	"gamma": {
 		"xl_type": "long",      # short/long/None
+		"frac_fp": 0.1,   # fraction of FP XLs
 		"pred_type": "guided",   # guided/unguided
 	}
 })
