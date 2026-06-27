49e5364ad7119655c0cc66ff5fd867b4f30be8e2
kenobi98 <majilakartik123@gmail.com>
2026-06-27 07:34:31 +0530

    yield_restraints(): extended allowed XLs to KRDEHQSTY

--- utils/mappings.py
+++ utils/mappings.py
@@ -85,6 +85,7 @@ def yield_restraints(
 		seq1 = entity_chain_map[entity_id1]["seq"]
 		seq2 = entity_chain_map[entity_id2]["seq"]
 
+		allowed_xl_aa = ["K", "R", "D", "E", "N", "Q", "S", "T", "Y"]
 		# For ambiguous XLs, we consider all combinations.
 		for chain_id1 in entity_chain_map[entity_id1]["chains"]:
 			if not numeric_chain_ids:
@@ -96,13 +97,13 @@ def yield_restraints(
 				if chain_id1 == chain_id2:
 					continue
 
-				if seq1[r1_idx] != "K":
+				if seq1[r1_idx] not in allowed_xl_aa:
 					raise ValueError( f"{sys_name}: Entity: {entity_id1}; " +
-						f"Chain: {chain_id1}; residue {res1} is not a Lys..." )
+						f"Chain: {chain_id1}; residue {res1}:{seq1[r1_idx]} is not among: {allowed_xl_aa}..." )
 
-				if seq2[r2_idx] != "K":
+				if seq2[r2_idx] not in allowed_xl_aa:
 					raise ValueError( f"{sys_name}: Entity: {entity_id2}; " +
-						f"Chain: {chain_id2}; residue {res2} is not a Lys..." )
+						f"Chain: {chain_id2}; residue {res2}:{seq2[r2_idx]} is not among: {allowed_xl_aa}..." )
 
 				yield entity_id1, entity_id2, chain_id1, chain_id2, res1, res2, label
 
